af02fa0e69fc4762ec9eefdc4e41f1a954fbabe6
ian@caret.cam.ac.uk <ian@caret.cam.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2006-09-07 22:45:57 +0000

    Initial Import
    git-svn-id: https://source.sakaiproject.org/contrib/tfd/trunk/hierarchy@1835 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ hierarchy-api/api/src/java/org/sakaiproject/hierarchy/api/HierarchyService.java
@@ -0,0 +1,60 @@
+package org.sakaiproject.hierarchy.api;
+
+import java.util.List;
+
+import org.sakaiproject.hierarchy.model.api.Hierarchy;
+import org.sakaiproject.hierarchy.model.api.HierarchyProperty;
+
+public interface HierarchyService
+{
+
+	/**
+	 * Get a list of Root nodes, the list contains Hierachy objects.
+	 * Root nodes represent the named hierachies that the hierachy service knos about
+	 * 
+	 * @return
+	 */
+	List getRootNodes();
+
+	/**
+	 * Get the node based on its nodePath
+	 * 
+	 * @param nodePath
+	 * @return
+	 */
+	Hierarchy getNode(String nodePath);
+
+	/**
+	 * delete nodes also removes the properties
+	 * 
+	 * @param nodePath
+	 */
+	void deleteNode(Hierarchy node);
+
+	/**
+	 * Save a node, and all connected nodes/properties
+	 * 
+	 * @param node
+	 */
+	void save(Hierarchy node);
+
+	/**
+	 * Create a new hierachy with a nodePath. The node path should represent the
+	 * nodes position withing the hierachy. It should also be unique within the
+	 * hierachy service. Once populated and connected, the node or an ancestor
+	 * will need to be saved.
+	 * 
+	 * @param nodePath
+	 * @return
+	 */
+	Hierarchy newHierarchy(String nodePath);
+
+	/**
+	 * create a new detached HierarchyPropery. This must be added to a node, and
+	 * the node saved
+	 * 
+	 * @return
+	 */
+	HierarchyProperty newHierachyProperty();
+
+}
