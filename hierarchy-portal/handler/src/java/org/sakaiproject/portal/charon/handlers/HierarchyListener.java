cbc38b7c5a8dc0b1e95fa2f23d6b47abde9bf3de
matthew.buckett@oucs.ox.ac.uk <matthew.buckett@oucs.ox.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-06-12 15:09:24 +0000

    Fixed hierarchy reset handler
    git-svn-id: https://source.sakaiproject.org/contrib/tetraelf/hierarchy/branches/sakai_2-5-x@50247 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- hierarchy-portal/handler/src/java/org/sakaiproject/portal/charon/handlers/HierarchyListener.java
+++ hierarchy-portal/handler/src/java/org/sakaiproject/portal/charon/handlers/HierarchyListener.java
@@ -11,6 +11,7 @@ import org.sakaiproject.site.api.SiteService;
 public class HierarchyListener implements ServletContextListener {
 
 	private PortalHandler hierarchyHandler;
+	private PortalHandler hierarchyResetHandler;
 	private PortalHandler magicHandler;
 	private PortalHandler hierarchyToolHandler;
 	private PortalHandler hierarchyToolResetHandler;
@@ -18,6 +19,7 @@ public class HierarchyListener implements ServletContextListener {
 	public void contextDestroyed(ServletContextEvent arg0) {
 		PortalService ps = (PortalService) org.sakaiproject.portal.api.cover.PortalService.getInstance();
 		ps.removeHandler("charon", hierarchyHandler.getUrlFragment());
+		ps.removeHandler("charon", hierarchyResetHandler.getUrlFragment());	
 		ps.removeHandler("charon", magicHandler.getUrlFragment());
 		ps.removeHandler("charon", hierarchyToolHandler.getUrlFragment());
 		ps.removeHandler("charon", hierarchyToolResetHandler.getUrlFragment());
@@ -28,10 +30,12 @@ public class HierarchyListener implements ServletContextListener {
 		SiteService siteService = (SiteService) org.sakaiproject.site.cover.SiteService.getInstance();
 		PortalHierarchyService portalHierarchyService = (PortalHierarchyService)org.sakaiproject.hierarchy.cover.PortalHierarchyService.getInstance();
 		hierarchyHandler = new HierarchyHandler(siteService, portalHierarchyService);
+		hierarchyResetHandler = new HierarchyResetHandler();
 		magicHandler = new MagicHandler();
 		hierarchyToolHandler = new HierarchyToolHandler();
 		hierarchyToolResetHandler = new HierarchyToolResetHandler();
 		ps.addHandler("charon", hierarchyHandler);
+		ps.addHandler("charon", hierarchyResetHandler);
 		ps.addHandler("charon", magicHandler);
 		ps.addHandler("charon", hierarchyToolHandler);
 		ps.addHandler("charon", hierarchyToolResetHandler);
