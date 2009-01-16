c23bc987a5fa0a8e6c16b435d35150d8e675a550
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-01-16 17:58:37 +0000

    Added hibernate impl of Dao

---
+++ impl/src/main/java/uk/ac/ox/oucs/vle/MappedGroupDaoHibernate.java
@@ -0,0 +1,40 @@
+package uk.ac.ox.oucs.vle;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class MappedGroupDaoHibernate extends HibernateDaoSupport implements
+		MappedGroupDao {
+	
+	private static Log log = LogFactory.getLog(MappedGroupDaoHibernate.class);
+
+	public MappedGroup findByGroupRole(String group, String role) {
+		MappedGroup example = new MappedGroup();
+		example.setExternalGroup(group);
+		example.setRole(role);
+		List<MappedGroup> results = getHibernateTemplate().findByExample(example);
+		MappedGroup result = null;
+		if (results.size() > 0) {
+			result = results.get(0);
+			if (results.size() > 1) {
+				log.warn("More than one result found for search: "+ example);
+			}
+		}
+		return result;
+	}
+
+	public MappedGroup findById(String id) {
+		return (MappedGroup) getHibernateTemplate().get(MappedGroup.class, id);
+	}
+
+	public String newMappedGroup(String group, String role) {
+		MappedGroup newGroup = new MappedGroup();
+		newGroup.setExternalGroup(group);
+		newGroup.setRole(role);
+		return (String) getHibernateTemplate().save(newGroup);
+	}
+
+}
