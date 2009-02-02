da6b3c4515c85238e0148d7c27e2f09b485cd2d7
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-02-02 16:04:56 +0000

    Switched to standard naming and add more site calls.

--- tool/src/main/java/uk/ac/ox/oucs/vle/SiteResource.java
+++ tool/src/main/java/uk/ac/ox/oucs/vle/SiteResource.java
@@ -12,23 +12,28 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ContextResolver;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.sakaiproject.authz.api.Role;
 import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.site.api.Site;
 import org.sakaiproject.site.api.SiteService;
+import org.sakaiproject.site.api.ToolConfiguration;
 
 @Path("/site/")
-public class SiteAddGroup {
+public class SiteResource {
 
+	private final static Log log = LogFactory.getLog(SiteResource.class);
+	
 	private SiteService siteService;
 	
-	public SiteAddGroup(@Context ContextResolver<Object> resolver) {
+	public SiteResource(@Context ContextResolver<Object> resolver) {
 		siteService = (SiteService) resolver.getContext(SiteService.class);
 	}
 
-	@Path("{site}/addgroup")
+	@Path("/{site}/addgroup")
 	@GET
 	public Response getSiteAddGroup(@PathParam("site") String siteId) {
 		Site site;
@@ -37,6 +42,26 @@ public class SiteAddGroup {
 		} catch (IdUnusedException idue) {
 			return Response.status(Status.NOT_FOUND).build();
 		}
+		return convertSiteToJSON(site);
+	}
+
+	@Path("/placement/{placement}")
+	@GET
+	public Response getSiteFromPlacement(@PathParam("placement") String placementId) {
+		Site site;
+		ToolConfiguration toolConfiguration = siteService.findTool(placementId);
+		try {
+			if (toolConfiguration != null) {
+				site = siteService.getSite(toolConfiguration.getSiteId());
+				return convertSiteToJSON(site);
+			}
+		} catch (IdUnusedException idue) {
+			log.warn("Placement had siteId that doesn't exist: "+ placementId);
+		}
+		return Response.status(Status.NOT_FOUND).build();
+	}
+
+	private Response convertSiteToJSON(Site site) {
 		Map<Object, Object> data = new HashMap<Object, Object>();
 		data.put("id", site.getId());
 		data.put("title", site.getTitle());
