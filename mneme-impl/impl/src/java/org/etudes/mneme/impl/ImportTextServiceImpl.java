3d733184febebb25151dc33cdadec2d5ddad4c86
Murthy Tanniru <murthy@etudes.org>
2009-02-06 21:37:51 +0000

    removed unwanted character in the code
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/mneme/trunk@57845 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- mneme-impl/impl/src/java/org/etudes/mneme/impl/ImportTextServiceImpl.java
+++ mneme-impl/impl/src/java/org/etudes/mneme/impl/ImportTextServiceImpl.java
@@ -20,7 +20,6 @@
  **********************************************************************************/
 
 package org.etudes.mneme.impl;
-`
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
