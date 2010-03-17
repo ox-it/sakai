83c453e35dc0f4409abb3e311a487c1d773216be
Glenn R. Golden <ggolden@etudes.org>
2010-03-17 01:20:27 +0000

    MN-979 0 next / prev for preview question
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/mneme/trunk@66598 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- mneme-tool/tool/src/java/org/etudes/mneme/tool/QuestionPreviewView.java
+++ mneme-tool/tool/src/java/org/etudes/mneme/tool/QuestionPreviewView.java
@@ -35,6 +35,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.etudes.ambrosia.api.Context;
 import org.etudes.ambrosia.util.ControllerImpl;
+import org.etudes.mneme.api.Assessment;
+import org.etudes.mneme.api.AssessmentService;
+import org.etudes.mneme.api.Pool;
+import org.etudes.mneme.api.PoolService;
 import org.etudes.mneme.api.Question;
 import org.etudes.mneme.api.QuestionService;
 import org.sakaiproject.util.StringUtil;
@@ -47,6 +51,12 @@ public class QuestionPreviewView extends ControllerImpl
 {
 	/** Our log. */
 	private static Log M_log = LogFactory.getLog(QuestionPreviewView.class);
+
+	/** Assessment service. */
+	protected AssessmentService assessmentService = null;
+
+	/** Pool Service */
+	protected PoolService poolService = null;
 
 	/** Question service. */
 	protected QuestionService questionService = null;
@@ -113,6 +123,20 @@ public class QuestionPreviewView extends ControllerImpl
 			List<Question> questions = new ArrayList<Question>();
 			questions.add(question);
 			context.put("questions", questions);
+
+			// returns from pool, or from assessment
+			// /pool_edit/0A/1/1A/1-30
+			if (destination.startsWith("/pool_edit"))
+			{
+				figurePrevNextForPoolEdit(context, destination, question);
+			}
+			// /assessment_edit/0A/1
+			else if (destination.startsWith("/assessment_edit"))
+			{
+				figurePrevNextForAssessmentEdit(context, destination, question);
+			}
+			
+			// TODO: /select_add_mpart_question/2/2/0A/1-30/0/0/B/assessment_edit/0A/2 support?
 		}
 
 		else
@@ -168,6 +192,26 @@ public class QuestionPreviewView extends ControllerImpl
 	}
 
 	/**
+	 * Set the AssessmentService.
+	 * 
+	 * @param service
+	 *        The AssessmentService.
+	 */
+	public void setAssessmentService(AssessmentService service)
+	{
+		this.assessmentService = service;
+	}
+
+	/**
+	 * @param poolService
+	 *        the poolService to set
+	 */
+	public void setPoolService(PoolService poolService)
+	{
+		this.poolService = poolService;
+	}
+
+	/**
 	 * Set the question service.
 	 * 
 	 * @param service
@@ -176,5 +220,118 @@ public class QuestionPreviewView extends ControllerImpl
 	public void setQuestionService(QuestionService service)
 	{
 		this.questionService = service;
+	}
+
+	/**
+	 * Figure the next and prev when coming from assessment edit
+	 * 
+	 * @param context
+	 *        The context.
+	 * @param destination
+	 *        The return path.
+	 * @param question
+	 *        The question.
+	 */
+	protected void figurePrevNextForAssessmentEdit(Context context, String destination, Question q)
+	{
+		// Note: must match the parameter and sort logic of AssessmentEditView
+		// /assessment_edit/0A/1
+
+		String[] params = StringUtil.split(destination, "/");
+		if (params.length < 4) return;
+
+		String assessmentId = params[3];
+		Assessment assessment = this.assessmentService.getAssessment(assessmentId);
+		if (assessment == null) return;
+
+		Question question = assessment.getParts().getQuestion(q.getId());
+		if (question == null) return;
+
+		Question prev = question.getAssessmentOrdering().getPrevious();
+		if (prev == null)
+		{
+			// TODO: getLastQuestion() support?
+			List<Question> questions = assessment.getParts().getQuestions();
+			prev = questions.get(questions.size() - 1);
+		}
+
+		Question next = question.getAssessmentOrdering().getNext();
+		if (next == null)
+		{
+			next = assessment.getParts().getFirst().getFirstQuestion();
+		}
+
+		if (prev != null) context.put("prevQuestionId", prev.getId());
+		if (next != null) context.put("nextQuestionId", next.getId());
+
+		context.put("position", question.getAssessmentOrdering().getPosition());
+		context.put("size", assessment.getParts().getNumQuestions());
+	}
+
+	/**
+	 * Figure the next and prev when coming from pool edit
+	 * 
+	 * @param context
+	 *        The context.
+	 * @param destination
+	 *        The return path.
+	 * @param question
+	 *        The question.
+	 */
+	protected void figurePrevNextForPoolEdit(Context context, String destination, Question question)
+	{
+		// Note: must match the parameter and sort logic of PoolEditView
+		// /pool_edit/0A/1/1A/1-30
+		String[] params = StringUtil.split(destination, "/");
+		if (params.length < 4) return;
+
+		// pool
+		String pid = params[3];
+		Pool pool = this.poolService.getPool(pid);
+		if (pool == null) return;
+
+		// sort
+		String sortCode = "0A";
+		if (params.length > 4) sortCode = params[4];
+		if ((sortCode == null) || (sortCode.length() != 2)) return;
+
+		// get questions
+		QuestionService.FindQuestionsSort sort = PoolEditView.findSort(sortCode);
+		List<Question> questions = questionService.findQuestions(pool, sort, null, null, null, null, null, null);
+
+		// figure this one's position (0 based)
+		int position = 0;
+		for (Question q : questions)
+		{
+			if (q.equals(question)) break;
+			position++;
+		}
+
+		// figure prev and next, w/ wrap
+		Question prev = null;
+		if (position > 0)
+		{
+			prev = questions.get(position - 1);
+		}
+		else
+		{
+			prev = questions.get(questions.size() - 1);
+		}
+
+		Question next = null;
+		if (position < questions.size() - 1)
+		{
+			next = questions.get(position + 1);
+		}
+		else
+		{
+			next = questions.get(0);
+		}
+
+		if (prev != null) context.put("prevQuestionId", prev.getId());
+		if (next != null) context.put("nextQuestionId", next.getId());
+
+		context.put("position", Integer.valueOf(position + 1));
+		context.put("size", Integer.valueOf(questions.size()));
 	}
 }
