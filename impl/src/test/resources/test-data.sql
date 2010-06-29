55d8a1f7d777483d4be47db481ba85c1233691eb
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-06-29 11:39:54 +0100

    Fixed SQL and got rid of course sets.

--- impl/src/test/resources/test-data.sql
+++ impl/src/test/resources/test-data.sql
@@ -1,14 +1,11 @@
 INSERT INTO course_group VALUES('course-1', 'The Politics of Brazil' );
 INSERT INTO course_group VALUES('course-2', 'The Politics of Mexico' );
 
-INSERT INTO course_component VALUES('comp-1', 'Lecture on Politics of Brazil', '2010-HIL', DATE_SUB('2010-10-10', 'INTERVAL 3 WEEK', DATE_SUB('2010-10-10', 'INTERVAL 1 WEEK', 40, 'tc-1');
-INSERT INTO course_component VALUES('comp-2', 'Lecture on Politics of Mexico', '2010-HIL', DATE_SUB('2010-10-10', 'INTERVAL 3 WEEK', DATE_SUB('2010-10-10', 'INTERVAL 1 WEEK', 45, 'tc-2');
-INSERT INTO course_component VALUES('comp-3', 'Seminar on South American Politics', '2010-HIL', DATE_SUB('2010-10-10', 'INTERVAL 3 WEEK', DATE_SUB('2010-10-10', 'INTERVAL 1 WEEK', 45, 'tc-3');
+INSERT INTO course_component VALUES('comp-1', 'Lecture on Politics of Brazil', '2010-HIL', DATE_SUB('2010-10-10', INTERVAL 3 WEEK), DATE_SUB('2010-10-10', INTERVAL 1 WEEK), 40, 'tc-1');
+INSERT INTO course_component VALUES('comp-2', 'Lecture on Politics of Mexico', '2010-HIL', DATE_SUB('2010-10-10', INTERVAL 3 WEEK), DATE_SUB('2010-10-10', INTERVAL 1 WEEK), 45, 'tc-2');
+INSERT INTO course_component VALUES('comp-3', 'Seminar on South American Politics', '2010-HIL', DATE_SUB('2010-10-10', INTERVAL 3 WEEK), DATE_SUB('2010-10-10', INTERVAL 1 WEEK), 45, 'tc-3');
 
-INSERT INTO course_set VALUES('set-1', 2, 'course-1');
-INSERT INTO course_set VALUES('set-2', 2, 'course-2');
-
-INSERT INTO course_set_component VALUES('set-1', 'comp-1');
-INSERT INTO course_set_component VALUES('set-1', 'comp-3');
-INSERT INTO course_set_component VALUES('set-2', 'comp-1');
-INSERT INTO course_set_component VALUES('set-2', 'comp-3');
+INSERT INTO course_group_component VALUES('course-1', 'comp-1');
+INSERT INTO course_group_component VALUES('course-1', 'comp-3');
+INSERT INTO course_group_component VALUES('course-2', 'comp-1');
+INSERT INTO course_group_component VALUES('course-2', 'comp-3');
