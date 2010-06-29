4baff65769132e549b0ad7e16151b769addd6490
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-06-29 15:41:00 +0100

    Add department associated with the course group.

--- impl/src/test/resources/test-data.sql
+++ impl/src/test/resources/test-data.sql
@@ -1,5 +1,5 @@
-INSERT INTO course_group VALUES('course-1', 'The Politics of Brazil' );
-INSERT INTO course_group VALUES('course-2', 'The Politics of Mexico' );
+INSERT INTO course_group VALUES('course-1', '3C05', 'The Politics of Brazil' );
+INSERT INTO course_group VALUES('course-2', '3C05', 'The Politics of Mexico' );
 
 INSERT INTO course_component VALUES('comp-1', 'Lecture on Politics of Brazil', '2010-HIL', DATE_SUB('2010-10-10', INTERVAL 3 WEEK), DATE_SUB('2010-10-10', INTERVAL 1 WEEK), 40, 0, 'tc-1');
 INSERT INTO course_component VALUES('comp-2', 'Lecture on Politics of Mexico', '2010-HIL', DATE_SUB('2010-10-10', INTERVAL 3 WEEK), DATE_SUB('2010-10-10', INTERVAL 1 WEEK), 45, 0, 'tc-2');
