485ae579f99d2460c796f70de3f1a0a4f19287e4
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-06-29 17:25:10 +0100

    Add the administrator.

--- impl/src/test/resources/test-data.sql
+++ impl/src/test/resources/test-data.sql
@@ -1,15 +1,15 @@
 INSERT INTO course_group VALUES('course-1', '3C05', 'The Politics of Brazil' );
 INSERT INTO course_group VALUES('course-2', '3C05', 'The Politics of Mexico' );
 
-INSERT INTO course_component VALUES('comp-1', 'Lecture on Politics of Brazil', '2010-HIL', DATE_SUB('2010-10-10', INTERVAL 3 WEEK), DATE_SUB('2010-10-10', INTERVAL 1 WEEK), 40, 0, 'tc-1');
-INSERT INTO course_component VALUES('comp-2', 'Lecture on Politics of Mexico', '2010-HIL', DATE_SUB('2010-10-10', INTERVAL 3 WEEK), DATE_SUB('2010-10-10', INTERVAL 1 WEEK), 45, 0, 'tc-2');
-INSERT INTO course_component VALUES('comp-3', 'Seminar on South American Politics', '2010-HIL', DATE_SUB('2010-10-10', INTERVAL 3 WEEK), DATE_SUB('2010-10-10', INTERVAL 1 WEEK), 45, 0, 'tc-3');
+INSERT INTO course_component VALUES('comp-1', 'Lecture on Politics of Brazil', '2010-HIL', DATE_SUB('2010-10-10', INTERVAL 3 WEEK), DATE_SUB('2010-10-10', INTERVAL 1 WEEK), 40, 0, 'd86d9720-eba4-40eb-bda3-91b3145729da', 'tc-1');
+INSERT INTO course_component VALUES('comp-2', 'Lecture on Politics of Mexico', '2010-HIL', DATE_SUB('2010-10-10', INTERVAL 3 WEEK), DATE_SUB('2010-10-10', INTERVAL 1 WEEK), 45, 0, 'd86d9720-eba4-40eb-bda3-91b3145729da', 'tc-2');
+INSERT INTO course_component VALUES('comp-3', 'Seminar on South American Politics', '2010-HIL', DATE_SUB('2010-10-10', INTERVAL 3 WEEK), DATE_SUB('2010-10-10', INTERVAL 1 WEEK), 45, 0, 'd86d9720-eba4-40eb-bda3-91b3145729da', 'tc-3');
 
-INSERT INTO course_component VALUES('comp-4', 'Lecture on Politics of Brazil', '2011-HIL', DATE_SUB('2011-10-10', INTERVAL 3 WEEK), DATE_SUB('2011-10-10', INTERVAL 1 WEEK), 40, 0, 'tc-1');
-INSERT INTO course_component VALUES('comp-5', 'Seminar on South American Politics', '2011-HIL', DATE_SUB('2011-10-10', INTERVAL 3 WEEK), DATE_SUB('2011-10-10', INTERVAL 1 WEEK), 45, 0, 'tc-3');
+INSERT INTO course_component VALUES('comp-4', 'Lecture on Politics of Brazil', '2011-HIL', DATE_SUB('2011-10-10', INTERVAL 3 WEEK), DATE_SUB('2011-10-10', INTERVAL 1 WEEK), 40, 0, 'd86d9720-eba4-40eb-bda3-91b3145729da', 'tc-1');
+INSERT INTO course_component VALUES('comp-5', 'Seminar on South American Politics', '2011-HIL', DATE_SUB('2011-10-10', INTERVAL 3 WEEK), DATE_SUB('2011-10-10', INTERVAL 1 WEEK), 45, 0, 'd86d9720-eba4-40eb-bda3-91b3145729da', 'tc-3');
 
-INSERT INTO course_component VALUES('comp-6', 'Lecture on Politics of Brazil', '2009-HIL', DATE_SUB('2009-10-10', INTERVAL 3 WEEK), DATE_SUB('2009-10-10', INTERVAL 1 WEEK), 40, 0, 'tc-1');
-INSERT INTO course_component VALUES('comp-7', 'Seminar on South American Politics', '2009-HIL', DATE_SUB('2009-10-10', INTERVAL 3 WEEK), DATE_SUB('2009-10-10', INTERVAL 1 WEEK), 45, 0, 'tc-3');
+INSERT INTO course_component VALUES('comp-6', 'Lecture on Politics of Brazil', '2009-HIL', DATE_SUB('2009-10-10', INTERVAL 3 WEEK), DATE_SUB('2009-10-10', INTERVAL 1 WEEK), 40, 0, 'd86d9720-eba4-40eb-bda3-91b3145729da', 'tc-1');
+INSERT INTO course_component VALUES('comp-7', 'Seminar on South American Politics', '2009-HIL', DATE_SUB('2009-10-10', INTERVAL 3 WEEK), DATE_SUB('2009-10-10', INTERVAL 1 WEEK), 45, 0, 'c10cdf4b-7c10-423c-8319-2d477051a94e', 'tc-3');
 
 INSERT INTO course_group_component VALUES('course-1', 'comp-1');
 INSERT INTO course_group_component VALUES('course-1', 'comp-3');
