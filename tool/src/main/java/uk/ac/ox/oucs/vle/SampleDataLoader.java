68927d6a30c813f76d6d89d65b8f9630ce35fdc9
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-07-01 14:43:57 +0100

    Standalone spring managed working.

---
+++ tool/src/main/java/uk/ac/ox/oucs/vle/SampleDataLoader.java
@@ -0,0 +1,48 @@
+package uk.ac.ox.oucs.vle;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class SampleDataLoader {
+
+	private SessionFactory factory;
+	private String dataFile = "/test-data.sql";
+
+	public void setSessionFactory(SessionFactory factory) {
+		this.factory = factory;
+	}
+
+	public void setDataFile(String dataFile) {
+		this.dataFile = dataFile;
+	}
+
+	public void init() throws Exception {
+
+		Session session = factory.openSession();
+		try {
+			InputStream stream = getClass().getResourceAsStream(
+					dataFile);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					stream));
+			StringBuilder buffer = new StringBuilder();
+			String line = null;
+			int semicolon;
+			while ((line = reader.readLine()) != null) {
+				buffer.append(line);
+				semicolon = buffer.indexOf(";");
+				if (semicolon > 0) {
+					String sql = buffer.substring(0, semicolon);
+					buffer.delete(0, semicolon + 1);
+					session.createSQLQuery(sql).executeUpdate();
+				}
+			}
+		} finally {
+			session.close();
+		}
+	}
+
+}
---
+++ tool/src/main/java/uk/ac/ox/oucs/vle/SpringComponentContextProvider.java
@@ -0,0 +1,29 @@
+package uk.ac.ox.oucs.vle;
+
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+@Provider
+public class SpringComponentContextProvider implements ContextResolver<Object> {
+
+	private ServletContext context;
+
+	public SpringComponentContextProvider(@Context ServletContext context) {
+		this.context = context;
+	}
+	
+	public Object getContext(Class<?> type) {
+		Map beans = WebApplicationContextUtils.getWebApplicationContext(context).getBeansOfType(type);
+		if (!beans.isEmpty()) {
+			return beans.values().iterator().next();
+		}
+		return null;
+	}
+
+}
