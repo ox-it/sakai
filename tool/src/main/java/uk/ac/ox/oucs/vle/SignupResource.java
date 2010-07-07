9aac835c1043e8ec160e56a20087d99f784ac81a
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-07-07 14:25:04 +0100

    Expose some of the signup data over HTTP/JSON.

---
+++ tool/src/main/java/uk/ac/ox/oucs/vle/SignupResource.java
@@ -0,0 +1,51 @@
+package uk.ac.ox.oucs.vle;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.StreamingOutput;
+import javax.ws.rs.ext.ContextResolver;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.type.TypeFactory;
+
+@Path("/signup")
+public class SignupResource {
+
+	
+	private CourseSignupService courseService;
+	private JsonFactory jsonFactory;
+	private ObjectMapper objectMapper;
+
+	public SignupResource(@Context ContextResolver<Object> resolver) {
+		this.courseService = (CourseSignupService) resolver.getContext(CourseSignupService.class);
+		jsonFactory = new JsonFactory();
+		objectMapper = new ObjectMapper();
+		objectMapper.configure(SerializationConfig.Feature.INDENT_OUTPUT, true);
+		objectMapper.configure(SerializationConfig.Feature.USE_STATIC_TYPING, true);
+		objectMapper.getSerializationConfig().setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+	}
+	
+	@Path("/all")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public StreamingOutput getMySignups() {
+		final List<CourseSignup> signups = courseService.getMySignups(null);
+		return new StreamingOutput() {
+			public void write(OutputStream output) throws IOException,
+					WebApplicationException {
+				objectMapper.typedWriter(TypeFactory.collectionType(List.class, CourseSignup.class)).writeValue(output, signups);
+			}
+		};
+	}
+}
