c8225e1fd44171387d6e5213794867c0ba8ef00c
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-07-27 12:20:55 +0100

    Overlay current users signups onto the signup page.

---
+++ tool/src/main/java/uk/ac/ox/oucs/vle/UserResource.java
@@ -0,0 +1,63 @@
+package uk.ac.ox.oucs.vle;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ContextResolver;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.node.ObjectNode;
+
+import uk.ac.ox.oucs.vle.proxy.SakaiProxy;
+import uk.ac.ox.oucs.vle.proxy.User;
+
+/**
+ * Class to allow details of the current user to be returned.
+ * At the moment this is used to get details of the current date so we don't rely on client clocks.
+ * @author buckett
+ *
+ */
+@Path("/user")
+public class UserResource {
+
+	private SakaiProxy proxy;
+	private CourseSignupService courseService;
+	private JsonFactory jsonFactory;
+	private ObjectMapper objectMapper;
+
+	public UserResource(@Context ContextResolver<Object> resolver) {
+		this.courseService = (CourseSignupService) resolver.getContext(CourseSignupService.class);
+		this.proxy = (SakaiProxy) resolver.getContext(SakaiProxy.class);
+		
+		jsonFactory = new JsonFactory();
+		objectMapper = new ObjectMapper();
+		objectMapper.configure(SerializationConfig.Feature.INDENT_OUTPUT, true);
+		objectMapper.configure(SerializationConfig.Feature.USE_STATIC_TYPING, true);
+		objectMapper.getSerializationConfig().setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+	}
+
+	@Path("/current")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getCurrent() throws JsonGenerationException, JsonMappingException, IOException {
+		User user = proxy.getCurrentUser();
+		Date now = courseService.getNow();
+		ObjectNode rootNode = objectMapper.createObjectNode();
+		rootNode.putPOJO("user", user);
+		rootNode.put("date", now.getTime());
+		return Response.ok(objectMapper.writeValueAsString(rootNode)).build();
+	}
+	
+	
+}
