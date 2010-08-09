da0fd65eef34ca7ee891c045a13f28d425e600d5
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-08-09 13:51:09 +0100

    Improve the formatting of the table and split common code.

--- tool/src/main/webapp/static/lib/signup.js
+++ tool/src/main/webapp/static/lib/signup.js
@@ -25,7 +25,58 @@ var Signup = function(){
                                 return Math.floor(remaining / 86400000) + " days";
                             }
             }
-        }
+        },
+		"signup": {
+			"getActions": function(status, id) {
+				switch (status) {
+					case "PENDING": 
+						return [
+							{
+								"name": "Accept",
+								"url": "/course-signup/rest/signup/"+id+"/accept"
+							},
+							{
+								"name": "Reject",
+								"url": "/course-signup/rest/signup/"+id+"/reject"
+							}
+						];
+					case "ACCEPTED":
+						return [
+							{
+								"name": "Approve",
+								"url": "/course-signup/rest/signup/"+id+"/approve"
+							},
+							{
+								"name": "Reject",
+								"url": "/course-signup/rest/signup/"+id+"/reject"
+							}
+						];
+					case "APPROVED":
+						return [];
+					case "REJECTED":
+						return [];
+					case "WITHDRAWN":
+						return [];
+				}
+				return [];
+			},
+			"formatActions": function(actions) {
+				return $.map(actions, function(action) {
+					return '<a class="action" href="'+ action.url+ '">'+ action.name+ '</a>';
+				}).join(" / ");
+			},
+			/**
+			 * Formats a notes string so we only display the first bit of it and then display a tooltip for the rest.
+			 * @param {Object} notes
+			 */
+			"formatNotes": function(notes) {
+				if (notes && notes.length> 50) {
+					return '<span class="signup-notes">'+ Text.toHtml(notes.substr(0, 45))+'... <span class="more">[more]<span class="full">'+ Text.toHtml(notes)+ '</span></span></span>'
+				} else {
+					return Text.toHtml(notes);
+				}
+			}
+		}
     };
 }();
 
