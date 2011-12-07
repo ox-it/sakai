fe29d22736a92c4cd8a808bd83e2b0948fc03072
Marc Savitsky <marc.savitsky@oucs.ox.ac.uk>
2011-12-07 13:15:26 +0000

    SESii WP31 Tool to run importer

---
+++ tool/src/main/java/uk/ac/ox/oucs/vle/SchedulerTool.java
@@ -0,0 +1,162 @@
+/*
+ * $Id$
+ **********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006, 2008 The Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package uk.ac.ox.oucs.vle;
+ 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.sakaiproject.api.app.scheduler.JobDetailWrapper;
+import org.sakaiproject.api.app.scheduler.SchedulerManager;
+import org.sakaiproject.api.app.scheduler.TriggerWrapper;
+import org.sakaiproject.component.app.scheduler.JobDetailWrapperImpl;
+import org.sakaiproject.component.app.scheduler.TriggerWrapperImpl;
+import org.sakaiproject.component.cover.ComponentManager;
+import org.sakaiproject.tool.api.Tool;
+
+public class SchedulerTool extends HttpServlet {
+
+  private static final Log LOG = LogFactory.getLog(SchedulerTool.class);
+  
+  private Scheduler scheduler;
+
+  public SchedulerTool()  {
+  }
+
+  /**
+   * 	
+   * @param scheduler
+   */
+  public List<JobDetailWrapper> processRefreshJobs() {
+  		
+	  List<JobDetailWrapper> jobDetailWrapperList = 
+			  new ArrayList<JobDetailWrapper>();
+  		
+  		try	{
+  			String[] jobNames = scheduler.getJobNames(Scheduler.DEFAULT_GROUP);
+  		
+  			for (int i = 0; i < jobNames.length; i++) {
+  				JobDetailWrapper jobDetailWrapper = new JobDetailWrapperImpl();
+  				jobDetailWrapper.setJobDetail(
+  						scheduler.getJobDetail(jobNames[i],	Scheduler.DEFAULT_GROUP));
+  				
+  				Trigger[] triggerArr = scheduler.getTriggersOfJob(jobNames[i],
+  						Scheduler.DEFAULT_GROUP);
+  				List<TriggerWrapper> triggerWrapperList = new ArrayList<TriggerWrapper>();
+  				TriggerWrapper tw;
+  				
+  				for (int j = 0; j < triggerArr.length; j++) {
+  					tw = new TriggerWrapperImpl();
+  					tw.setTrigger(triggerArr[j]);
+  					triggerWrapperList.add(tw);
+  				}
+
+  				jobDetailWrapper.setTriggerWrapperList(triggerWrapperList);
+  				jobDetailWrapperList.add(jobDetailWrapper);
+  			}
+  			
+  		} catch (SchedulerException e) {
+  			LOG.error("scheduler error while getting job detail");
+  		}
+  		
+  		return jobDetailWrapperList;
+  	}
+  
+  	/**
+  	 * This method runs the current job only once, right now
+  	 * @return String
+  	 */
+  	public String processRunJobNow(String jobName) {
+  		
+  		try	{
+  			List<JobDetailWrapper> jobDetailWrapperList =
+  					processRefreshJobs();
+  			
+  			JobDetailWrapper selectedJobDetailWrapper = null;
+  			
+  			for (JobDetailWrapper jobDetailWrapper : jobDetailWrapperList) {
+  	  			if (jobDetailWrapper.getJobDetail().getName().equals(jobName)) {
+  	  				selectedJobDetailWrapper = jobDetailWrapper;
+  	  			}
+  	  		}
+  			
+  			scheduler.triggerJob(
+  					selectedJobDetailWrapper.getJobDetail().getName(), 
+  					selectedJobDetailWrapper.getJobDetail().getGroup());
+
+  			return "success";
+  			
+  		} catch (Exception e) {
+  			LOG.error("Failed to trigger job now");
+  			return "error";
+  		}
+  	}
+  	
+  	private void setScheduler() {
+  		SchedulerManager schedulerManager = 
+  				(SchedulerManager) ComponentManager.getInstance().get(
+  						org.sakaiproject.api.app.scheduler.SchedulerManager.class);
+  		
+  		scheduler = schedulerManager.getScheduler();
+  		if (scheduler == null) {
+  			LOG.error("Scheduler is down!");
+  		}
+  	}
+  
+  	public void doGet(HttpServletRequest request, HttpServletResponse response)
+  			throws ServletException, IOException {
+  		
+  		ServletContext context = getServletContext();
+  		setScheduler();
+  		List<JobDetailWrapper> jobDetailWrapperList =
+					processRefreshJobs();
+  		
+  		request.setAttribute(Tool.NATIVE_URL, Tool.NATIVE_URL);
+  		request.setAttribute("jobDetailList", jobDetailWrapperList);
+  		
+  		RequestDispatcher dispatcher = 
+  				context.getRequestDispatcher("/static/quartz.jsp");
+  		dispatcher.forward( request, response );
+  	}
+  	
+  	public void doPost(HttpServletRequest request, HttpServletResponse response)
+  			throws ServletException, IOException {
+  		
+  		Map<String, String[]> requestParams = request.getParameterMap();
+  		String[] vals = (String[])requestParams.get("jobName");  
+  		String jobName = vals[0];
+  		setScheduler();
+  		processRunJobNow(jobName);
+  	}
+}
+
---
+++ tool/src/main/webapp/static/quartz.jsp
@@ -0,0 +1,76 @@
+<%@ page import="org.sakaiproject.tool.cover.ToolManager"%>
+<%@ page import="org.sakaiproject.component.cover.ServerConfigurationService" %>
+<%@ page import="org.sakaiproject.user.cover.UserDirectoryService" %>
+<%@ page session="false" %> 
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core"  prefix="c" %>
+
+<%
+pageContext.setAttribute("jobName", ServerConfigurationService.getString("ses.import.job", "gobbledegoup"));
+%>
+<c:set var="jobName" value="${jobName}" />
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<!-- Make the page render as IE8 for wrapping in jstree -->
+	<meta http-equiv="X-UA-Compatible" content="IE=8" >
+	
+	<title>Module Signup</title>
+ 
+	<link href="<%= ServerConfigurationService.getString("skin.repo", "/library/skin") %>/tool_base.css" type="text/css" rel="stylesheet" media="all" />
+	<link href="<%= ServerConfigurationService.getString("skin.repo", "/library/skin") %>/<%= ServerConfigurationService.getString("skin.default", "default") %>/tool.css" type="text/css" rel="stylesheet" media="all" />
+	
+    </head>
+    <body>
+    	<div>
+    	
+		<div id="messages">
+        </div>
+		
+        <div id="browse">
+        	<form method="post">
+        		<img alt="" src="/course-signup/static/images/quartz.jpg">
+        		
+        		<fieldset>
+        			<c:forEach var="jobDetailWrapper" items="${jobDetailList}">
+        				<c:choose>
+        				<c:when test="${jobDetailWrapper.jobDetail.name == jobName}">
+        					<input type="radio" name="jobName" 
+        						value="<c:out value="${jobDetailWrapper.jobDetail.name}"/>" checked >
+							<span style="font-weight:bolder">
+								<c:out value="${jobDetailWrapper.jobDetail.name}" />
+							</span>
+						</c:when>
+						<c:otherwise>
+        					<input type="radio" name="jobName" 
+        						value="<c:out value="${jobDetailWrapper.jobDetail.name}" />" disabled>
+							<span style="font-weight:lighter">
+								<c:out value="${jobDetailWrapper.jobDetail.name}" />
+							</span>
+						</c:otherwise>
+						</c:choose>
+						<br>
+					</c:forEach>
+        		</fieldset>
+        		
+        		<fieldset>
+        			<legend>Run Job Now Confirmation:  <c:out value="${jobName}" /></legend>
+        			<table class="chefEditItem" cellspacing="0">
+						<tbody>
+						<tr>
+						<td class="chefLabel">Are you sure you would like to run the job now?</td>
+						</tr>
+						</tbody>
+					</table>
+        		</fieldset>
+        		
+        		<p>
+					<input type="submit" value="Run Now" />
+				</p>
+			</form>
+		</div>
+		
+		</div>
+    </body>
+</html>
