b03699df32288ecf0f535bcda41c0953e215061e
Rashmi Maheshwari <rashmi@etudes.org>
2012-04-02 18:53:00 +0000

    copyright license revise
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/etudes-util/trunk@79100 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- etudes-util/util/src/java/org/etudes/util/HtmlHelper.java
+++ etudes-util/util/src/java/org/etudes/util/HtmlHelper.java
@@ -3,7 +3,7 @@
  * $Id$
  ***********************************************************************************
  *
- * Copyright (c) 2010, 2011 Etudes, Inc.
+ * Copyright (c) 2010, 2011, 2012 Etudes, Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
