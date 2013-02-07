8f7298db614abefe8cf680226f774a3d087fec3f
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2013-02-07 11:18:02 +0000

    Fix indentation.

--- hierarchy-tool/tool/src/test/org/sakaiproject/hierarchy/tool/vm/AddRedirectControllerTest.java
+++ hierarchy-tool/tool/src/test/org/sakaiproject/hierarchy/tool/vm/AddRedirectControllerTest.java
@@ -41,78 +41,78 @@ import org.springframework.web.servlet.DispatcherServlet;
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestExecutionListeners(AutowiringTestExecutionListener.class)
 @ContextConfiguration(locations={"classpath:test-resources.xml", "classpath:applicationContext.xml"},
-		loader=MockWebApplicationContextLoader.class)
+loader=MockWebApplicationContextLoader.class)
 @MockWebApplication(name="sakai.hierarchy-manager", webapp="src/webapp")
 @Configurable(autowire=Autowire.BY_TYPE)
 public class AddRedirectControllerTest {
-	
-    @Autowired
-    private DispatcherServlet servlet;
-    
-    @Autowired
-    private PortalHierarchyService portalHierarchyService;
-    
-    @Autowired
-    private SessionManager sessionManager;
-    
-    public void setServlet(DispatcherServlet servlet) {
+
+	@Autowired
+	private DispatcherServlet servlet;
+
+	@Autowired
+	private PortalHierarchyService portalHierarchyService;
+
+	@Autowired
+	private SessionManager sessionManager;
+
+	public void setServlet(DispatcherServlet servlet) {
 		this.servlet = servlet;
 	}
 
-    public void setPortalHierarchyService(
+	public void setPortalHierarchyService(
 			PortalHierarchyService portalHierarchyService) {
 		this.portalHierarchyService = portalHierarchyService;
 	}
 
-    public void setSessionManager(SessionManager sessionManager) {
+	public void setSessionManager(SessionManager sessionManager) {
 		this.sessionManager = sessionManager;
 	}
 
 	@Before
-    public void setUp() {
+	public void setUp() {
 		Site site = mock(Site.class);
 		when(site.getId()).thenReturn("site-id");
 		when(site.getTitle()).thenReturn("Site Title");
 		when(site.getShortDescription()).thenReturn("Short Description");
-		
-    	PortalNodeSite node = mock(PortalNodeSite.class);
-    	when(node.getId()).thenReturn("id");
-    	when(node.getName()).thenReturn("name");
-    	when(node.getPath()).thenReturn("/name");
-    	when(node.getSite()).thenReturn(site);
-    	when(portalHierarchyService.getCurrentPortalNode()).thenReturn(node);
-    	
-    	Session session = mock(Session.class);
-    	when(sessionManager.getCurrentSession()).thenReturn(session);
-    }
 
-	@Test
-	public void testManageHome() throws ServletException, IOException {
-        MockHttpServletRequest request = new MockHttpServletRequest("GET", "");
-        // As in sakai the path info is where we do our controller routing from.
-        request.setPathInfo("/");
-        MockHttpServletResponse response = new MockHttpServletResponse();
-        servlet.service(request, response);
-        assertEquals(200, response.getStatus());
+		PortalNodeSite node = mock(PortalNodeSite.class);
+		when(node.getId()).thenReturn("id");
+		when(node.getName()).thenReturn("name");
+		when(node.getPath()).thenReturn("/name");
+		when(node.getSite()).thenReturn(site);
+		when(portalHierarchyService.getCurrentPortalNode()).thenReturn(node);
+
+		Session session = mock(Session.class);
+		when(sessionManager.getCurrentSession()).thenReturn(session);
 	}
 
 	@Test
-    public void testForm() throws ServletException, IOException {
-        MockHttpServletRequest request = new MockHttpServletRequest("GET", "");
-        // As in sakai the path info is where we do our controller routing from.
-        request.setPathInfo("/redirect/add");
-        MockHttpServletResponse response = new MockHttpServletResponse();
-        servlet.service(request, response);
-        assertEquals(200, response.getStatus());
-    }
-	
+	public void testManageHome() throws ServletException, IOException {
+		MockHttpServletRequest request = new MockHttpServletRequest("GET", "");
+		// As in sakai the path info is where we do our controller routing from.
+		request.setPathInfo("/");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		servlet.service(request, response);
+		assertEquals(200, response.getStatus());
+	}
+
+	@Test
+	public void testForm() throws ServletException, IOException {
+		MockHttpServletRequest request = new MockHttpServletRequest("GET", "");
+		// As in sakai the path info is where we do our controller routing from.
+		request.setPathInfo("/redirect/add");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		servlet.service(request, response);
+		assertEquals(200, response.getStatus());
+	}
+
 	@Test
 	public void testSubmit() throws ServletException, IOException, PermissionException {
 		MockHttpServletRequest request = new MockHttpServletRequest("POST", "");
 		request.setPathInfo("/redirect/add");
 		MockHttpServletResponse response = new MockHttpServletResponse();
 		servlet.service(request, response);
-		
+
 		assertEquals(200, response.getStatus());
 		assertTrue(response.getContentAsString().contains("Title cannot be empty."));
 		// Check that validation should have stopped the request.
