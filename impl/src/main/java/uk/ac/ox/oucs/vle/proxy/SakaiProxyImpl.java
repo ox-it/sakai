701d1053c54f7b0956fa8c491d42b3c9023eed27
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2013-02-08 12:08:56 +0000

    WL-2506 Have one aes() method which does both encryption/description.
    This reduces code duplication.

--- impl/src/main/java/uk/ac/ox/oucs/vle/proxy/SakaiProxyImpl.java
+++ impl/src/main/java/uk/ac/ox/oucs/vle/proxy/SakaiProxyImpl.java
@@ -323,7 +323,7 @@ public class SakaiProxyImpl implements SakaiProxy {
 	}
 	
 	public String encode(String uncoded) {
-		byte[] encrypted = encrypt(uncoded.getBytes());
+		byte[] encrypted = aes(uncoded.getBytes(), Cipher.ENCRYPT_MODE);
 		if (encrypted != null) {
 			String base64String = new String(Base64.encodeBase64(encrypted));
 			return base64String.replace('+','-').replace('/','_');
@@ -336,7 +336,7 @@ public class SakaiProxyImpl implements SakaiProxy {
 	public String uncode(String encoded) {
 		String base64String = encoded.replace('-','+').replace('_','/');
 		byte[] encrypted = Base64.decodeBase64(base64String.getBytes());
-		String decrypted = new String(decrypt(encrypted));
+		String decrypted = new String(aes(encrypted, Cipher.DECRYPT_MODE));
 		// On failed decryption we have to return null.
 		return decrypted;
 	}
@@ -375,6 +375,7 @@ public class SakaiProxyImpl implements SakaiProxy {
 
 	protected String getSecretKey() {
 		// Return null if not set.
+		// TODO This should do length checking as AES has fixed key lenths.
 		String key = serverConfigurationService.getString("aes.secret.key", null);
 		if (key == null) {
 			log.error("No secret key specified. Please set 'aes.secret.key' in configuration");
@@ -382,7 +383,7 @@ public class SakaiProxyImpl implements SakaiProxy {
 		return key;
 	}
 	
-	protected byte[] encrypt(byte[] source) {
+	protected byte[] aes(byte[] source, int mode) {
 		String secretKey = getSecretKey();
 		if (secretKey == null) {
 			return null;
@@ -391,29 +392,15 @@ public class SakaiProxyImpl implements SakaiProxy {
 		try {
 			// Instantiate the cipher
 			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-			cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+			cipher.init(mode, skeySpec);
 			byte[] encrypted = cipher.doFinal(source);
 			return encrypted;
 		
 		} catch (Exception e) {
-			log.warn("Encryption failed.", e);
-		}
-		return null;
-	}
-	
-	protected byte[] decrypt(byte[] source) {
-		String secretKey = getSecretKey();
-		if (secretKey == null) {
-			return null;
-		}
-		SecretKeySpec skeySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
-		try {
-			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-			cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-			byte[] original = cipher.doFinal(source);
-			return original;
-		} catch (Exception e) {
-			log.warn("Decription failed.", e); 
+			String type = (mode == Cipher.DECRYPT_MODE)? "decryption" :
+				(mode == Cipher.ENCRYPT_MODE)? "encryption" :
+				"unknown";
+			log.warn("AES "+ type+ " failed.", e);
 		}
 		return null;
 	}
