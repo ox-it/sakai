e74fa370afdfdae242c08c66376dd5ee7cc1a708
Marc Savitsky <marc.savitsky@it.ox.ac.uk>
2013-03-06 09:30:58 +0000

    WL-2269 added @Override annotation

--- tool/src/main/java/uk/ac/ox/oucs/vle/mvc/PathInfoHelper.java
+++ tool/src/main/java/uk/ac/ox/oucs/vle/mvc/PathInfoHelper.java
@@ -10,6 +10,7 @@ public class PathInfoHelper extends UrlPathHelper {
 	 * Sakai wrapper around request returns pathInfo of null
 	 * which breaks spring mvc
 	 */
+	@Override
 	public String getLookupPathForRequest(HttpServletRequest request) {
 		String lookup = request.getPathInfo();
 		if (lookup == null)
