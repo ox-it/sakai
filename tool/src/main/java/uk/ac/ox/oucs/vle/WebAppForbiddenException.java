75e66999b75ac7ace62b8e7e34ad34076e108023
Marc Savitsky <marc.savitsky@it.ox.ac.uk>
2013-03-08 11:39:46 +0000

    WL-2627 use static initialisers of entity

--- tool/src/main/java/uk/ac/ox/oucs/vle/WebAppForbiddenException.java
+++ tool/src/main/java/uk/ac/ox/oucs/vle/WebAppForbiddenException.java
@@ -1,19 +1,23 @@
 package uk.ac.ox.oucs.vle;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class WebAppForbiddenException extends WebApplicationException {
 	
+	private static final Map<String, String> myMap = new HashMap<String, String>();
+	static {
+		myMap.put("status", "failed");
+		myMap.put("message", "Not Authorized");
+	}
+
 	public WebAppForbiddenException() {
 		super(Response
 				.status(Response.Status.FORBIDDEN)
-				.entity(new HashMap<String,String>() {{
-					put("status", "failed");
-					put("message", "Not Authorized");}})
+				.entity(myMap)
 				.build());
 	}
 
--- tool/src/main/java/uk/ac/ox/oucs/vle/WebAppNotFoundException.java
+++ tool/src/main/java/uk/ac/ox/oucs/vle/WebAppNotFoundException.java
@@ -1,19 +1,23 @@
 package uk.ac.ox.oucs.vle;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class WebAppNotFoundException extends WebApplicationException {
 	
+	private static final Map<String, String> myMap = new HashMap<String, String>();
+	static {
+		myMap.put("status", "failed");
+		myMap.put("message", "The requested resource was not found");
+	}
+	
 	public WebAppNotFoundException() {
 		super(Response
 				.status(Response.Status.NOT_FOUND)
-				.entity(new HashMap<String,String>() {{
-					put("status", "failed");
-					put("message", "The requested resource was not found");}})
+				.entity(myMap)
 				.build());
 	}
 
