e76a6197074ab1899e4e51c7e1d4fe137bb33ad9
Marc Savitsky <marc.savitsky@it.ox.ac.uk>
2013-03-08 11:51:19 +0000

    WL-2627 use static maps

--- tool/src/main/java/uk/ac/ox/oucs/vle/CustomExceptionMapper.java
+++ tool/src/main/java/uk/ac/ox/oucs/vle/CustomExceptionMapper.java
@@ -1,6 +1,7 @@
 package uk.ac.ox.oucs.vle;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -17,19 +18,27 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class CustomExceptionMapper implements ExceptionMapper<CourseSignupException> {
 
+	private static final Map<String, String> forbiddenMap = new HashMap<String, String>();
+	static {
+		forbiddenMap.put("status", "failed");
+		forbiddenMap.put("message", "Not Authorized");
+	}
+	
+	private static final Map<String, String> notFoundMap = new HashMap<String, String>();
+	static {
+		notFoundMap.put("status", "failed");
+		notFoundMap.put("message", "The requested resource was not found");
+	}
+	
 	public Response toResponse(CourseSignupException exception) {
 		if(exception instanceof NotFoundException) {
 			return Response.status(Status.NOT_FOUND)
-					.entity(new HashMap<String,String>() {{
-						put("status", "failed");
-						put("message", "The requested resource was not found");}})
-						.build();
+					.entity(notFoundMap)
+					.build();
 		} else if (exception instanceof PermissionDeniedException) {
 			return Response.status(Status.FORBIDDEN)
-					.entity(new HashMap<String,String>() {{
-						put("status", "failed");
-						put("message", "Not Authorized");}})
-						.build();
+					.entity(forbiddenMap)
+					.build();
 		}
 		
 		return Response.status(Status.INTERNAL_SERVER_ERROR).build();
