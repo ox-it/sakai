670a1b21828b7cda3f1dbfb4f9bff29d46e34d81
Marc Savitsky <marc.savitsky@it.ox.ac.uk>
2013-03-14 16:52:03 +0000

    WL-2627 comment and empty tabs removed

--- tool/src/main/java/uk/ac/ox/oucs/vle/SignupResource.java
+++ tool/src/main/java/uk/ac/ox/oucs/vle/SignupResource.java
@@ -104,7 +104,6 @@ public class SignupResource {
 			throw new WebAppForbiddenException();
 		}
 		CourseSignup entity = courseService.signup(courseId, components, email, message);
-		//ResponseBuilder builder = new ResponseBuilderImpl();
 		ResponseBuilder builder = Response.status(Response.Status.CREATED);
 		builder.status(201);
 		builder.entity(entity);
