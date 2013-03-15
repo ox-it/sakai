c2714045f9a080c3b2d6c4008a4538e7a5608f6c
Marc Savitsky <marc.savitsky@it.ox.ac.uk>
2013-03-15 15:49:47 +0000

    WL-2763 set request attributes for course.admin

--- tool/src/main/java/uk/ac/ox/oucs/vle/SchedulerTool.java
+++ tool/src/main/java/uk/ac/ox/oucs/vle/SchedulerTool.java
@@ -19,7 +19,7 @@
  **********************************************************************************/
 
 package uk.ac.ox.oucs.vle;
- 
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +39,7 @@ import org.quartz.Trigger;
 import org.sakaiproject.api.app.scheduler.JobDetailWrapper;
 import org.sakaiproject.api.app.scheduler.SchedulerManager;
 import org.sakaiproject.api.app.scheduler.TriggerWrapper;
+import org.sakaiproject.component.api.ServerConfigurationService;
 import org.sakaiproject.component.app.scheduler.JobDetailWrapperImpl;
 import org.sakaiproject.component.app.scheduler.TriggerWrapperImpl;
 import org.sakaiproject.component.cover.ComponentManager;
@@ -46,116 +47,123 @@ import org.sakaiproject.tool.api.Tool;
 
 public class SchedulerTool extends HttpServlet {
 
-  private static final Log LOG = LogFactory.getLog(SchedulerTool.class);
-  
-  private Scheduler scheduler;
+	private static final Log LOG = LogFactory.getLog(SchedulerTool.class);
 
-  public SchedulerTool()  {
-  }
+	private Scheduler scheduler;
 
-  /**
-   * 	
-   * @param scheduler
-   */
-  public List<JobDetailWrapper> processRefreshJobs() {
-  		
-	  List<JobDetailWrapper> jobDetailWrapperList = 
-			  new ArrayList<JobDetailWrapper>();
-  		
-  		try	{
-  			String[] jobNames = scheduler.getJobNames(Scheduler.DEFAULT_GROUP);
-  		
-  			for (int i = 0; i < jobNames.length; i++) {
-  				JobDetailWrapper jobDetailWrapper = new JobDetailWrapperImpl();
-  				jobDetailWrapper.setJobDetail(
-  						scheduler.getJobDetail(jobNames[i],	Scheduler.DEFAULT_GROUP));
-  				Trigger[] triggerArr = scheduler.getTriggersOfJob(jobNames[i],
-  						Scheduler.DEFAULT_GROUP);
-  				List<TriggerWrapper> triggerWrapperList = new ArrayList<TriggerWrapper>();
-  				TriggerWrapper tw;
-  				
-  				for (int j = 0; j < triggerArr.length; j++) {
-  					tw = new TriggerWrapperImpl();
-  					tw.setTrigger(triggerArr[j]);
-  					triggerWrapperList.add(tw);
-  				}
+	public SchedulerTool() {
+	}
 
-  				jobDetailWrapper.setTriggerWrapperList(triggerWrapperList);
-  				jobDetailWrapperList.add(jobDetailWrapper);
-  			}
-  			
-  		} catch (SchedulerException e) {
-  			LOG.error("scheduler error while getting job detail");
-  		}
-  		
-  		return jobDetailWrapperList;
-  	}
-  
-  	/**
-  	 * This method runs the current job only once, right now
-  	 * @return String
-  	 */
-  	public String processRunJobNow(String jobName) {
-  		
-  		try	{
-  			List<JobDetailWrapper> jobDetailWrapperList =
-  					processRefreshJobs();
-  			
-  			JobDetailWrapper selectedJobDetailWrapper = null;
-  			
-  			for (JobDetailWrapper jobDetailWrapper : jobDetailWrapperList) {
-  	  			if (jobDetailWrapper.getJobDetail().getName().equals(jobName)) {
-  	  				selectedJobDetailWrapper = jobDetailWrapper;
-  	  			}
-  	  		}
-  			
-  			scheduler.triggerJob(
-  					selectedJobDetailWrapper.getJobDetail().getName(), 
-  					selectedJobDetailWrapper.getJobDetail().getGroup());
+	/**
+	 * 
+	 * @param scheduler
+	 */
+	public List<JobDetailWrapper> processRefreshJobs() {
 
-  			return "success";
-  			
-  		} catch (Exception e) {
-  			LOG.error("Failed to trigger job now");
-  			return "error";
-  		}
-  	}
-  	
-  	private void setScheduler() {
-  		SchedulerManager schedulerManager = 
-  				(SchedulerManager) ComponentManager.getInstance().get(
-  						org.sakaiproject.api.app.scheduler.SchedulerManager.class);
-  		
-  		scheduler = schedulerManager.getScheduler();
-  		if (scheduler == null) {
-  			LOG.error("Scheduler is down!");
-  		}
-  	}
-  
-  	public void doGet(HttpServletRequest request, HttpServletResponse response)
-  			throws ServletException, IOException {
-  		
-  		ServletContext context = getServletContext();
-  		setScheduler();
-  		List<JobDetailWrapper> jobDetailWrapperList =
-					processRefreshJobs();
-  		
-  		request.setAttribute(Tool.NATIVE_URL, Tool.NATIVE_URL);
-  		request.setAttribute("jobDetailList", jobDetailWrapperList);
-  		
-  		RequestDispatcher dispatcher = 
-  				context.getRequestDispatcher("/static/quartz.jsp");
-  		dispatcher.forward( request, response );
-  	}
-  	
-  	public void doPost(HttpServletRequest request, HttpServletResponse response)
-  			throws ServletException, IOException {
-  		
-  		Map<String, String[]> requestParams = request.getParameterMap();
-  		String[] vals = (String[])requestParams.get("jobName");  
-  		String jobName = vals[0];
-  		setScheduler();
-  		processRunJobNow(jobName);
-  	}
+		List<JobDetailWrapper> jobDetailWrapperList = new ArrayList<JobDetailWrapper>();
+
+		try {
+			String[] jobNames = scheduler.getJobNames(Scheduler.DEFAULT_GROUP);
+
+			for (int i = 0; i < jobNames.length; i++) {
+				JobDetailWrapper jobDetailWrapper = new JobDetailWrapperImpl();
+				jobDetailWrapper.setJobDetail(scheduler.getJobDetail(
+						jobNames[i], Scheduler.DEFAULT_GROUP));
+				Trigger[] triggerArr = scheduler.getTriggersOfJob(jobNames[i],
+						Scheduler.DEFAULT_GROUP);
+				List<TriggerWrapper> triggerWrapperList = new ArrayList<TriggerWrapper>();
+				TriggerWrapper tw;
+
+				for (int j = 0; j < triggerArr.length; j++) {
+					tw = new TriggerWrapperImpl();
+					tw.setTrigger(triggerArr[j]);
+					triggerWrapperList.add(tw);
+				}
+
+				jobDetailWrapper.setTriggerWrapperList(triggerWrapperList);
+				jobDetailWrapperList.add(jobDetailWrapper);
+			}
+
+		} catch (SchedulerException e) {
+			LOG.error("scheduler error while getting job detail");
+		}
+
+		return jobDetailWrapperList;
+	}
+
+	/**
+	 * This method runs the current job only once, right now
+	 * 
+	 * @return String
+	 */
+	public String processRunJobNow(String jobName) {
+
+		try {
+			List<JobDetailWrapper> jobDetailWrapperList = processRefreshJobs();
+
+			JobDetailWrapper selectedJobDetailWrapper = null;
+
+			for (JobDetailWrapper jobDetailWrapper : jobDetailWrapperList) {
+				if (jobDetailWrapper.getJobDetail().getName().equals(jobName)) {
+					selectedJobDetailWrapper = jobDetailWrapper;
+				}
+			}
+
+			scheduler.triggerJob(selectedJobDetailWrapper.getJobDetail()
+					.getName(), selectedJobDetailWrapper.getJobDetail()
+					.getGroup());
+
+			return "success";
+
+		} catch (Exception e) {
+			LOG.error("Failed to trigger job now");
+			return "error";
+		}
+	}
+
+	private void setScheduler() {
+		SchedulerManager schedulerManager = (SchedulerManager) ComponentManager
+				.getInstance()
+				.get(org.sakaiproject.api.app.scheduler.SchedulerManager.class);
+
+		scheduler = schedulerManager.getScheduler();
+		if (scheduler == null) {
+			LOG.error("Scheduler is down!");
+		}
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ServletContext context = getServletContext();
+		setScheduler();
+		List<JobDetailWrapper> jobDetailWrapperList = processRefreshJobs();
+
+		request.setAttribute(Tool.NATIVE_URL, Tool.NATIVE_URL);
+		request.setAttribute("jobDetailList", jobDetailWrapperList);
+
+		ServerConfigurationService service = 
+				(ServerConfigurationService) ComponentManager.get("org.sakaiproject.component.api.ServerConfigurationService");
+		
+		request.setAttribute("skinRepo", 
+				service.getString("skin.repo", "/library/skin"));
+
+		request.setAttribute("skinDefault",
+				service.getString("skin.default", "default"));
+
+		RequestDispatcher dispatcher = context
+				.getRequestDispatcher("/static/quartz.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Map<String, String[]> requestParams = request.getParameterMap();
+		String[] vals = (String[]) requestParams.get("jobName");
+		String jobName = vals[0];
+		setScheduler();
+		processRunJobNow(jobName);
+	}
 }
 
