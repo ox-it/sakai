9ad3474d547f9e5fe347d89711ec3a89e498e5b8
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2013-04-02 15:59:39 +0100

    WL-2355 Add the license to all our files.

--- impl/src/main/resources/test-data.sql
+++ impl/src/main/resources/test-data.sql
@@ -1,3 +1,22 @@
+-- -
+-- #%L
+-- Course Signup Implementation
+-- %%
+-- Copyright (C) 2010 - 2013 University of Oxford
+-- %%
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--             http://opensource.org/licenses/ecl2
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- #L%
+-- -
 INSERT INTO course_group(courseId, dept, title) VALUES('course-1', '3C05', 'The Politics of Brazil' );
 INSERT INTO course_group(courseId, dept, title) VALUES('course-2', '3C05', 'The Politics of Mexico' );
 INSERT INTO course_group(courseId, dept, title) VALUES('course-3', '3C05', 'Testing of Open' );
