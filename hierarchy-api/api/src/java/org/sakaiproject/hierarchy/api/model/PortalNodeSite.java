c3a149b5b53973bcd29df6bcdcd95186ab0a0b61
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2013-05-02 12:27:20 +0100

    Cleanup JavaDoc.

--- hierarchy-api/api/src/java/org/sakaiproject/hierarchy/api/model/PortalNodeSite.java
+++ hierarchy-api/api/src/java/org/sakaiproject/hierarchy/api/model/PortalNodeSite.java
@@ -5,6 +5,7 @@ import org.sakaiproject.site.api.Site;
 /**
  * This is a node that we store in the tree which indicates there 
  * is a site here.
+ * 
  * @author buckett
  *
  */
@@ -12,13 +13,14 @@ public interface PortalNodeSite extends PortalNode {
 	
 	/**
 	 * The site associated with the current node.
-	 * @return The site, this will never by <code>null</code>
+	 * @return The site, this will never be <code>null</code>.
 	 */
 	public Site getSite();
 	
 	/**
 	 * The management site is the site which contains all the management tools for
 	 * the portal. Typically it has a site ID of !hierarchy.
+	 * @return The management site, this will never be <code>null</code>.
 	 */
 	public Site getManagementSite();
 }
