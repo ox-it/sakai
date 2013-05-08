dcf8313bb1811239f377b2dcc977af9dfc0f615a
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2013-05-08 12:13:13 +0100

    WL-2819 Autocreate indexes on site ID and hash.
    This is because hibernate doesn't do index autocreation.

--- hierarchy-impl/impl/src/java/org/sakaiproject/hierarchy/impl/portal/dao/PortalPersistentNodeDaoHibernate.java
+++ hierarchy-impl/impl/src/java/org/sakaiproject/hierarchy/impl/portal/dao/PortalPersistentNodeDaoHibernate.java
@@ -4,14 +4,58 @@ package org.sakaiproject.hierarchy.impl.portal.dao;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.HibernateException;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 public class PortalPersistentNodeDaoHibernate extends HibernateDaoSupport implements PortalPersistentNodeDao {
 	
+	private static final Log log = LogFactory.getLog(PortalPersistentNodeDaoHibernate.class);
+
+	private static final String INDEX_SITE_ID = "create index portal_node_site_id_idx on PORTAL_NODE (siteId)";
+	private static final String INDEX_PATH_HASH = "create unique index portal_node_path_hash_idx on PORTAL_NODE (pathHash)";
+
+	private boolean ddl = true;
+
+	public void setDdl(boolean ddl) {
+		this.ddl = ddl;
+	}
+
+	public void init() {
+		if (ddl) {
+			Session session = getSession();
+			Transaction ta = session.beginTransaction();
+			SQLQuery createSQLQuery;
+			try {
+				// Hibernate fails to create this index so we do it manually.
+				// With later versions of hibernate this can go away.
+				createSQLQuery = session.createSQLQuery(INDEX_SITE_ID);
+				createSQLQuery.executeUpdate();
+				createSQLQuery = session.createSQLQuery(INDEX_PATH_HASH);
+				createSQLQuery.executeUpdate();
+				ta.commit();
+				log.info("Created indexes.");
+			} catch (HibernateException e ) {
+				// This will fail on all subsequent restarts
+				// On MySQL at least this comes through as a SQL Grammar exception so it isn't easy to
+				// see when it's failing due to index already existing.
+				log.debug("Failed to create index: "+ e.getMessage() );
+				ta.rollback();
+				session.close();
+			}
+		}
+	}
+
+	@Override
 	public PortalPersistentNode findById(String id) {
 		return (PortalPersistentNode)getHibernateTemplate().get(PortalPersistentNode.class, id);
 	}
-	
+
+	@Override
 	public PortalPersistentNode findByPathHash(String pathHash){
 		return (PortalPersistentNode) justFirst(getHibernateTemplate()
 				.find(
@@ -19,13 +63,16 @@ public class PortalPersistentNodeDaoHibernate extends HibernateDaoSupport implem
 						pathHash));
 	}
 
+	@SuppressWarnings("unchecked")
+	@Override
 	public List<PortalPersistentNode> findBySiteId(String siteId) {
-		return (List<PortalPersistentNode>) getHibernateTemplate()
+		return getHibernateTemplate()
 				.find(
 						"from org.sakaiproject.hierarchy.impl.portal.dao.PortalPersistentNode as node where node.siteId = ?",
 						siteId);
 	}
-	
+
+	@Override
 	public void save(PortalPersistentNode node) {
 		Date now = new Date();
 		if (node.getCreated() == null) {
@@ -34,7 +81,7 @@ public class PortalPersistentNodeDaoHibernate extends HibernateDaoSupport implem
 		node.setUpdated(now);
 		getHibernateTemplate().saveOrUpdate(node);
 	}
-	
+
 	private Object justFirst(List<?> list) {
 		if (list != null && list.size() > 0) {
 			return list.get(0);			
@@ -42,6 +89,7 @@ public class PortalPersistentNodeDaoHibernate extends HibernateDaoSupport implem
 		return null;
 	}
 
+	@Override
 	public void delete(String id) {
 		PortalPersistentNode node = findById(id);
 		getHibernateTemplate().delete(node);
