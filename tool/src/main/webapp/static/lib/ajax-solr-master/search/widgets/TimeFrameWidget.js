6ef17109db9b6bd8d9c5e94ac7863d3f06bb849a
Marc Savitsky <marc.savitsky@it.ox.ac.uk>
2013-06-20 09:08:56 +0100

    WL-2365 Stashed while I work on other things

--- tool/src/main/webapp/static/lib/ajax-solr-master/search/widgets/TimeFrameWidget.js
+++ tool/src/main/webapp/static/lib/ajax-solr-master/search/widgets/TimeFrameWidget.js
@@ -12,25 +12,27 @@ AjaxSolr.TimeFrameWidget = AjaxSolr.AbstractFacetWidget.extend({
 	var objectedItems = [];
 	var before = parseInt(this.manager.response.facet_counts.facet_ranges.course_basedate.before);
 	if (before > 0) {
-		objectedItems.push({ facet: "Old Courses", count: before });
+		objectedItems.push({ facet: "Old Courses", count: before, query: "[* TO NOW]", field: "course_basedate" });
 	}
 	var after = parseInt(this.manager.response.facet_counts.facet_ranges.course_basedate.after);
 	if (after > 0) {
-		objectedItems.push({ facet: "Current Courses", count: after });
+		objectedItems.push({ facet: "Current Courses", count: after, query: "[NOW TO *]", field: "course_basedate" });
 	}
 	var count = parseInt(this.manager.response.facet_counts.facet_ranges.course_created.counts[1]);
 	if (count > 0) {
-		objectedItems.push({ facet: "New Courses", count: count });
+		objectedItems.push({ facet: "New Courses", count: count, query: "[NOW-14DAY TO NOW]", field: "course_created" });
 	}
 	
 	$(this.target).empty();
 	for (var i = 0, l = objectedItems.length; i < l; i++) {
 		var facet = objectedItems[i].facet;
 		var count = objectedItems[i].count;
+		var query = objectedItems[i].query;
+		var field = objectedItems[i].field;
 		$(this.target).append(
 				$('<a href="#" class="tagcloud_item"></a>')
 				.text(facet+" ("+count+")")
-				.click(this.clickHandler(facet))
+				.click(this.clickHandler(field, query))
 		);
 	}
 
@@ -40,7 +42,57 @@ AjaxSolr.TimeFrameWidget = AjaxSolr.AbstractFacetWidget.extend({
 	}
 	var thisHeight = myHeight+'em';
 	document.getElementById(this.id).style.height = thisHeight;
-  }
-});
+  },
 
+
+/**
+ * @param {String} value The value.
+ * @returns {Function} Sends a request to Solr if it successfully adds a
+ *   filter query with the given value.
+ */
+
+  clickHandler: function (field, query) {
+	var self = this, meth = this.multivalue ? 'add' : 'set';
+	return function () {
+		if (self[meth].call(self, value)) {
+			self.doRequest();
+		}
+		return false;
+	}
+  },
+  
+  /**
+   * Sets the filter query.
+   *
+   * @returns {Boolean} Whether the selection changed.
+   */
+  set: function (value) {
+    return this.changeSelection(function () {
+      var a = this.manager.store.removeByValue('fq', new RegExp('^-?' + this.field + ':')),
+          b = this.manager.store.addByValue('fq', this.fq(value));
+      return a || b;
+    });
+  },
+
+  /**
+   * Adds a filter query.
+   *
+   * @returns {Boolean} Whether a filter query was added.
+   */
+  add: function (value) {
+    return this.changeSelection(function () {
+      return this.manager.store.addByValue('fq', this.fq(value));
+    });
+  },
+  
+  /**
+   * @param {String} value The facet value.
+   * @param {Boolean} exclude Whether to exclude this fq parameter value.
+   * @returns {String} An fq parameter value.
+   */
+  fq: function (value, exclude) {
+    return (exclude ? '-' : '') + this.field + ':' + AjaxSolr.Parameter.escapeValue(value);
+  }
+  
+})
 })(jQuery);
