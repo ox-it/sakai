5d678bce99b15681fce6be3fd847999907edd1f4
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2013-08-01 15:24:42 +0100

    Convert all tabs to spaces.
    This is the default for ajax-solr lib.

--- tool/src/main/webapp/static/lib/ajax-solr-master/search/widgets/ResultWidget.js
+++ tool/src/main/webapp/static/lib/ajax-solr-master/search/widgets/ResultWidget.js
@@ -1,9 +1,9 @@
 (function ($) {
 
 AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
-	
+
   start: 0,
-  
+
   beforeRequest: function () {
     $(this.target).html($('<img>').attr('src', 'images/loader.gif'));
   },
@@ -58,112 +58,112 @@ AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
       }
     }
 
-	$("#result form").submit(function(e) {
-		e.preventDefault();
-		try {
-			var form = this;
-			var id = $("input[name=id]", form).val();
-			var range = $("input[name=previous]", form).val();
-			var workingWindow = parent.window || window;
-			var position = Signup.util.dialogPosition();
-			var height = Math.round($(workingWindow).height() * 0.9);
-			var width = Math.round($(window).width() * 0.9);
-								
-			var courseDetails = $("<div></div>").dialog({
-				autoOpen: false,
-				stack: true,
-				position: position,
-				width: width,
-				height: height,
-				modal: true,
-				close: function(event, ui){
-					courseDetails.remove(); /* Tidy up the DOM. */
-				}
-			});
-			
-			Signup.course.show(courseDetails, id, range, externalUser, "../rest", function(){
-				courseDetails.dialog("open");
-			});
-		} catch (e) {
-			console.log(e);
-		}
-	});
+    $("#result form").submit(function(e) {
+        e.preventDefault();
+        try {
+            var form = this;
+            var id = $("input[name=id]", form).val();
+            var range = $("input[name=previous]", form).val();
+            var workingWindow = parent.window || window;
+            var position = Signup.util.dialogPosition();
+            var height = Math.round($(workingWindow).height() * 0.9);
+            var width = Math.round($(window).width() * 0.9);
+
+            var courseDetails = $("<div></div>").dialog({
+                autoOpen: false,
+                stack: true,
+                position: position,
+                width: width,
+                height: height,
+                modal: true,
+                close: function(event, ui){
+                    courseDetails.remove(); /* Tidy up the DOM. */
+                }
+            });
+
+            Signup.course.show(courseDetails, id, range, externalUser, "../rest", function(){
+                courseDetails.dialog("open");
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    });
   },
 
   template: function (doc) {
-	var now = new Date();
-	var range = "ALL";
-	var signup_message = "";
-	var booking_message = "";
-	var close = Signup.util.parseDate(doc.course_signup_close);
-	var open = Signup.util.parseDate(doc.course_signup_open);
-	
-	// Set range to UPCOMING, PREVIOUS or ALL depending on current selection 
-	var previous = false;
-	var upcoming = false;
-	var fq = this.manager.store.values('fq');
-	for (var i = 0, l = fq.length; i < l; i++) {
-		var facet = this.manager.getValueName(fq[i]);
-		if (facet == 'Old Courses') {
-			previous = true;
-		}
-		if (facet == 'Current Courses') {
-			upcoming = true;
-		}
-	}
-	
-	if (previous && !upcoming) {
-		range = "PREVIOUS";
-	}
-	if (!previous && upcoming) {
-		range = "UPCOMING";
-	}
-	
-	if (isNaN(open.getDate()) || isNaN(close.getDate())) {
-		if (doc.course_signup_opentext) {
-			signup_message = doc.course_signup_opentext;
-		}
-	} else {
-	
-		if (close < now) {
-			signup_message = "closed";
-		} else {
-			if (open > now) {
-				signup_message = "opens in " + Signup.util.formatDuration(open -now);
-			} else {
-				signup_message = "closes in " + Signup.util.formatDuration(close -now);
-			}
-		}
-	}
-	
-	var output = '<div id="doc"><form class="details"><strong>' + doc.course_title + '</strong>';
-	output += ',&nbsp;&nbsp;'+doc.provider_title;
-	if (signup_message) {
-    		output += '&nbsp;(signup '+signup_message+ ')';
+    var now = new Date();
+    var range = "ALL";
+    var signup_message = "";
+    var booking_message = "";
+    var close = Signup.util.parseDate(doc.course_signup_close);
+    var open = Signup.util.parseDate(doc.course_signup_open);
+
+    // Set range to UPCOMING, PREVIOUS or ALL depending on current selection 
+    var previous = false;
+    var upcoming = false;
+    var fq = this.manager.store.values('fq');
+    for (var i = 0, l = fq.length; i < l; i++) {
+        var facet = this.manager.getValueName(fq[i]);
+        if (facet == 'Old Courses') {
+            previous = true;
+        }
+        if (facet == 'Current Courses') {
+            upcoming = true;
+        }
     }
-	output += '<p id="links_' + doc.course_identifier + '" class="links"></p>';
-	output += '<div id="description"><a href="javascript:{}" class="more">Show description</a>&nbsp;&nbsp;';
-	output += '<div class="toggle" style="display:none;">' + doc.course_description+'<br /></div>';
-	output += '<input type="hidden" name="id" value="' + doc.course_identifier + '">';
-	output += '<input type="hidden" name="previous" value="'+range+'">';
-	output += '<input type="submit" value="More details">';
-	output += '</div>';
-	output += '</form></div>';
-	return output;
+
+    if (previous && !upcoming) {
+        range = "PREVIOUS";
+    }
+    if (!previous && upcoming) {
+        range = "UPCOMING";
+    }
+
+    if (isNaN(open.getDate()) || isNaN(close.getDate())) {
+        if (doc.course_signup_opentext) {
+            signup_message = doc.course_signup_opentext;
+        }
+    } else {
+
+        if (close < now) {
+            signup_message = "closed";
+        } else {
+            if (open > now) {
+                signup_message = "opens in " + Signup.util.formatDuration(open -now);
+            } else {
+                signup_message = "closes in " + Signup.util.formatDuration(close -now);
+            }
+        }
+    }
+
+    var output = '<div id="doc"><form class="details"><strong>' + doc.course_title + '</strong>';
+    output += ',&nbsp;&nbsp;'+doc.provider_title;
+    if (signup_message) {
+            output += '&nbsp;(signup '+signup_message+ ')';
+    }
+    output += '<p id="links_' + doc.course_identifier + '" class="links"></p>';
+    output += '<div id="description"><a href="javascript:{}" class="more">Show description</a>&nbsp;&nbsp;';
+    output += '<div class="toggle" style="display:none;">' + doc.course_description+'<br /></div>';
+    output += '<input type="hidden" name="id" value="' + doc.course_identifier + '">';
+    output += '<input type="hidden" name="previous" value="'+range+'">';
+    output += '<input type="submit" value="More details">';
+    output += '</div>';
+    output += '</form></div>';
+    return output;
   },
-  
+
   init: function () {
     $(document).on('click', 'a.more', function () {
       var $this = $(this),
           toggle = $this.parent().find('div.toggle');
 
       if (toggle.is(':visible')) {
-    	  toggle.hide();
+          toggle.hide();
         $this.text('Show description');
       }
       else {
-    	$this.text('Hide description');
-    	toggle.show();
+        $this.text('Hide description');
+        toggle.show();
       }
       return false;
     });
