c74246dcf9f4dd1a1eb1c1eadff22f0e551eacb6
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2013-11-22 11:49:43 +0000

    WL-3015 Correctly name my test method.
    Transitive is when you have 3 objects.

--- impl/src/test/java/uk/ac/ox/oucs/vle/TermCodeComparatorTest.java
+++ impl/src/test/java/uk/ac/ox/oucs/vle/TermCodeComparatorTest.java
@@ -14,29 +14,29 @@ public class TermCodeComparatorTest {
 
 	@Test
 	public void testDiffYear() {
-		assertTransative("HT13", "HT12");
+		assertSymmetric("HT13", "HT12");
 	}
 
 	@Test
 	public void testDiffTerm() {
-		assertTransative("TT13", "MT13");
+		assertSymmetric("TT13", "MT13");
 	}
 
 	@Test
 	public void testDiff() {
-		assertTransative("MT13", "TT11");
+		assertSymmetric("MT13", "TT11");
 	}
 
 	@Test
 	public void testTermOrder() {
-		assertTransative("HT10", "MT10");
-		assertTransative("TT10", "MT10");
-		assertTransative("TT10", "HT10");
+		assertSymmetric("HT10", "MT10");
+		assertSymmetric("TT10", "MT10");
+		assertSymmetric("TT10", "HT10");
 	}
 
 	@Test
 	public void testNullTerm() {
-		assertTransative(null, "HT10");
+		assertSymmetric(null, "HT10");
 	}
 
 	@Test
@@ -46,20 +46,20 @@ public class TermCodeComparatorTest {
 
 	@Test
 	public void testBadTerm() {
-		assertTransative("II10", "HT10");
+		assertSymmetric("II10", "HT10");
 	}
 
 	@Test
 	public void testBadData() {
-		assertTransative("WXYZ", "ABCD");
+		assertSymmetric("WXYZ", "ABCD");
 	}
 
 	@Test
 	public void testBadDataNull() {
-		assertTransative("ABCD", null);
+		assertSymmetric("ABCD", null);
 	}
 
-	private void assertTransative(String tc1, String tc2) {
+	private void assertSymmetric(String tc1, String tc2) {
 		assertTrue(tc1+ " should be before "+ tc2, comp.compare(tc1, tc2) > 0);
 		assertTrue(tc2+ " should be after "+ tc1, comp.compare(tc2, tc1) < 0);
 	}
