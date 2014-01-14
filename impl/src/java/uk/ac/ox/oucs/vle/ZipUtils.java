e81e9f73fc2b61eae9e15f7d1b9b29aa2e99fe4a
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2014-01-14 10:54:25 +0000

    Add the ability to create a zip file.
    This code isn't yet used.

--- impl/src/java/uk/ac/ox/oucs/vle/ZipUtils.java
+++ impl/src/java/uk/ac/ox/oucs/vle/ZipUtils.java
@@ -1,16 +1,22 @@
 package uk.ac.ox.oucs.vle;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.IOUtils;
 
@@ -66,5 +72,59 @@ public class ZipUtils {
 			errors.add(new ZipError(null, "Failed to get next entry"));
 		}
 		return errors;
+	}
+	
+	/**
+	 * Creates an input based on a folder and all it's contents
+	 * @param outputstream The outputstream is write the filezip to.
+	 * @param root The path the zip.
+	 * @return A list of errors.
+	 */
+	public static List<ZipError> createZip(OutputStream outputStream, String root) {
+		List<ZipError> errors = new ArrayList<ZipError>();
+		Queue<File> dirsToProcess = new ArrayDeque<File>(); 
+		ZipOutputStream zipfile = new ZipOutputStream(outputStream);
+
+		File rootFile = new File(root);
+		dirsToProcess.add(rootFile);
+
+		File dir;
+		while ((dir = dirsToProcess.poll()) != null) {
+			if (dir.exists()) {
+				for (File file: dir.listFiles()) {
+					if (file.isDirectory()) {
+						dirsToProcess.add(file);
+					} else {
+						BufferedInputStream input = null;
+						try {
+							String zipName = relative(rootFile, file);
+							ZipEntry entry = new ZipEntry(zipName);
+							zipfile.putNextEntry(entry);
+							input = new BufferedInputStream(new FileInputStream(file));
+							IOUtils.copy(input, zipfile);
+						} catch (FileNotFoundException fne) {
+							errors.add(new ZipError(file, "Couldn't find file"));
+						} catch (IOException ioe) {
+							errors.add(new ZipError(file, "Problem reading file."));
+						}
+						finally {
+							if (input != null) {
+								try {
+									input.close();
+								} catch (IOException ioe) {} //Ignore
+							}
+						}
+					}
+				}
+			} else {
+				errors.add(new ZipError(dir, "Doesn't exist"));
+			}
+		}
+
+		return errors;
+	}
+	
+	static String relative (File parent, File child) {
+		return child.getAbsolutePath().substring(parent.getAbsolutePath().length());
 	}
 }
