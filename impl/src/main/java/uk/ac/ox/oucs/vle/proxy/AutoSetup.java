613d22764a1eac27261cfae23792dcc0baae3664
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2014-07-01 17:08:48 +0100

    Auto import departments into content hosting.
    This means we already have the departments hierarchy inside Sakai when
    testing. This is true by default as it shouldn’t have an effect if the 
    files are already there.

--- impl/src/main/java/uk/ac/ox/oucs/vle/proxy/AutoSetup.java
+++ impl/src/main/java/uk/ac/ox/oucs/vle/proxy/AutoSetup.java
@@ -1,115 +1,168 @@
-/*
- * #%L
- * Course Signup Implementation
- * %%
- * Copyright (C) 2010 - 2013 University of Oxford
- * %%
- * Licensed under the Educational Community License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *             http://opensource.org/licenses/ecl2
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 package uk.ac.ox.oucs.vle.proxy;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.quartz.JobDataMap;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.sakaiproject.api.app.scheduler.JobBeanWrapper;
-import org.sakaiproject.api.app.scheduler.SchedulerManager;
 import org.sakaiproject.component.api.ServerConfigurationService;
-import org.sakaiproject.component.app.scheduler.jobs.SpringJobBeanWrapper;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.sakaiproject.content.api.ContentCollectionEdit;
+import org.sakaiproject.content.api.ContentHostingService;
+import org.sakaiproject.content.api.ContentResourceEdit;
+import org.sakaiproject.entity.api.Entity;
+import org.sakaiproject.entity.api.ResourceProperties;
+import org.sakaiproject.event.api.UsageSessionService;
+import org.sakaiproject.exception.*;
+import org.sakaiproject.tool.api.Session;
+import org.sakaiproject.tool.api.SessionManager;
+import org.sakaiproject.util.SakaiComponentEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+import java.io.InputStream;
 
 /**
- * This class performs automatic setup of the SES tool.
- * The main purpose of this is to automatically run the importers on startup.
- * @author buckett
- *
+ * This attempts to automatically setup content needed for the tool to function.
+ * This is done on Spring events so that we can be sure all the services have done init()
+ * and are ready for use.
  */
-public class AutoSetup implements ApplicationContextAware {
+public class AutoSetup implements ApplicationListener {
 
 	private static final Log log = LogFactory.getLog(AutoSetup.class);
 
-	private SchedulerManager schedulerManager;
-	public void setSchedulerManager(SchedulerManager schedulerManager) {
-		this.schedulerManager = schedulerManager;
-	}
-
+	private ContentHostingService contentHostingService;
+	private SessionManager sessionManager;
+	private UsageSessionService usageSessionService;
 	private ServerConfigurationService serverConfigurationService;
-	public void setServerConfigurationService(
-			ServerConfigurationService serverConfigurationService) {
+
+	public void setServerConfigurationService(ServerConfigurationService serverConfigurationService) {
 		this.serverConfigurationService = serverConfigurationService;
 	}
-	
-	private ApplicationContext applicationContext;
-	public void setApplicationContext(ApplicationContext applicationContext)
-			throws BeansException {
-		this.applicationContext = applicationContext;
-		
+
+	public void setContentHostingService(ContentHostingService contentHostingService) {
+		this.contentHostingService = contentHostingService;
 	}
 
-	public void init() {
-		if (serverConfigurationService.getBoolean("ses.autosetup", false)) {
-			log.info ("SES AutoSetup running");
-			Scheduler scheduler = schedulerManager.getScheduler();
-			
-			String[] startups = serverConfigurationService.getStrings("ses.startup");
-			for (String startup : startups) {
-				try {
-					String jobBean = serverConfigurationService.getString("ses.import."+startup+".jobbean");
-					SpringJobBeanWrapper job = (SpringJobBeanWrapper)applicationContext.getBean(jobBean);
-					JobDataMap jobData = new JobDataMap();
-					jobData.put(JobBeanWrapper.SPRING_BEAN_NAME, job.getBeanId());
-					jobData.put(JobBeanWrapper.JOB_TYPE, job.getJobType());
-					
-					JobDataMap triggerData = new JobDataMap();
-					
-					String param;
-					param = serverConfigurationService.getString("ses.import."+startup+".uri");
-					if (null != param) {
-						triggerData.put("xcri.oxcap.populator.uri", param);
-					}
-					
-					param = serverConfigurationService.getString("ses.import."+startup+".user");
-					if (null != param) {
-						triggerData.put("xcri.oxcap.populator.username", param);
-					}
-					
-					param = serverConfigurationService.getString("ses.import."+startup+".password");
-					if (null != param) {
-						triggerData.put("xcri.oxcap.populator.password", param);
-					}
-					
-					param = serverConfigurationService.getString("ses.import."+startup+".name");
-					if (null != param) {
-						triggerData.put("xcri.oxcap.populator.name", param);
-					}
-					
-					JobDetail jobDetail = new JobDetail(job.getJobType(), null, job.getJobClass());
-					jobDetail.setJobDataMap(jobData);
-					scheduler.addJob(jobDetail, true);
-					scheduler.triggerJobWithVolatileTrigger(job.getJobType(), null, triggerData);
-					log.info("Triggered job: "+ job.getJobType());
-				} catch (SchedulerException se) {
-					log.warn("Failed to run job: "+ startup, se);
-				}
-				
+	public void setSessionManager(SessionManager sessionManager) {
+		this.sessionManager = sessionManager;
+	}
+
+	public void setUsageSessionService(UsageSessionService usageSessionService) {
+		this.usageSessionService = usageSessionService;
+	}
+
+	@Override
+	public void onApplicationEvent(ApplicationEvent event) {
+
+		if (serverConfigurationService.getBoolean("ses.autosetup", true) &&
+				event instanceof SakaiComponentEvent) {
+			try {
+				doAsAdmin(new ImportContent());
+			} catch (Exception e) {
+				log.error("Failed to setup content", e);
 			}
 		}
 	}
 
-	
+	/**
+	 * Actually so the importing.
+	 */
+	protected class ImportContent implements Runnable {
+
+		@Override
+		public void run() {
+			String siteId = serverConfigurationService.getString("course-signup.site-id", "course-signup");
+			String collection = ContentHostingService.COLLECTION_SITE + siteId;
+			// Create folder.
+			createFolder(ContentHostingService.COLLECTION_SITE, siteId, true);
+			// Copy in files
+			copyFile(collection, "departments.json");
+		}
+
+		/**
+		 * Copy a file from the classpath to content hosting.
+		 * Don't do anything if the file already exists.
+		 *
+		 * @param collection The location to import into.
+		 * @param filename   The filename
+		 */
+		private void copyFile(String collection, String filename) {
+			String classPath = "/autosetup/" + filename;
+			InputStream source = getClass().getResourceAsStream(classPath);
+			if (source == null) {
+				throw new NullPointerException("Couldn't find " + source);
+			}
+			String contentPath = collection + Entity.SEPARATOR+ filename;
+			try {
+				ContentResourceEdit file = contentHostingService.addResource(contentPath);
+				file.getProperties().addProperty(ResourceProperties.PROP_DISPLAY_NAME, filename);
+				file.setContent(source);
+				contentHostingService.commitResource(file);
+			} catch (PermissionException e) {
+				throw new RuntimeException(e);
+			} catch (IdUsedException e) {
+				log.debug("File already exists: " + contentPath);
+			} catch (IdInvalidException e) {
+				throw new RuntimeException(e);
+			} catch (InconsistentException e) {
+				throw new RuntimeException(e);
+			} catch (ServerOverloadException e) {
+				throw new RuntimeException(e);
+			} catch (OverQuotaException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		/**
+		 * Create a folder in content hosting.
+		 * If the folder already exists don't do anything.
+		 *
+		 * @param base The containing folder.
+		 * @param name The name of the new folder
+		 * @param isPublic If <code>true</code> then make it public
+		 */
+		private void createFolder(String base, String name, boolean isPublic) {
+			String path = base + name;
+			try {
+				ContentCollectionEdit collection = contentHostingService.addCollection(base, name);
+				collection.getProperties().addProperty(ResourceProperties.PROP_DISPLAY_NAME, name);
+				if (isPublic) {
+					collection.setPublicAccess();
+				}
+				contentHostingService.commitCollection(collection);
+			} catch (IdUsedException e) {
+				log.debug("Folder already exists, this is expected: " + path);
+			} catch (IdInvalidException e) {
+				throw new RuntimeException(e);
+			} catch (PermissionException e) {
+				throw new RuntimeException(e);
+			} catch (InconsistentException e) {
+				throw new RuntimeException(e);
+			} catch (IdUnusedException e) {
+				throw new RuntimeException(e);
+			} catch (TypeException e) {
+				throw new RuntimeException(e);
+			} catch (IdLengthException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	/**
+	 * Runs the supplied instance as admin.
+	 * But makes sure then no matter what happens we stop running as admin at the end.
+	 *
+	 * @param runnable The thing to run as admin.
+	 */
+	protected void doAsAdmin(Runnable runnable) {
+		try {
+			Session session = sessionManager.startSession();
+			session.setUserId("admin");
+			sessionManager.setCurrentSession(session);
+			usageSessionService.startSession("admin", null, null);
+			runnable.run();
+		} finally {
+			Session session = sessionManager.getCurrentSession();
+			if (session != null) {
+				session.invalidate();
+			}
+		}
+	}
 }
