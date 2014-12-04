c42be1e793a56265e745071fa5fe35c5c20e7f12
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2014-12-04 17:06:17 +0000

    Initial import.

---
+++ src/java/LoadServlet.java
@@ -0,0 +1,82 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.component.cover.ComponentManager;
+import org.sakaiproject.tool.api.SessionManager;
+
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * This is a servlet that is slowed down as more session are allocated to the server.
+ */
+@WebServlet(name="load",
+        urlPatterns={"/*"},
+        initParams={ })
+public class LoadServlet extends HttpServlet {
+
+    private final Log log = LogFactory.getLog(LoadServlet.class);
+
+    private int sleepLimit = 5000; // Don't sleep for more than 5 seconds.
+    private int sessionAge = 1200; // 20 minutes.
+    private int sessionSleep = 10; // Sleep for 10ms per session.
+
+    private SessionManager sessionManager;
+
+    @Override
+    public void init() {
+        sessionManager = (SessionManager) ComponentManager.get(SessionManager.class);
+        sleepLimit = tryParseInt(getServletConfig().getInitParameter("sleepLimit"), sleepLimit);
+        sessionAge = tryParseInt(getServletConfig().getInitParameter("sessionAge"), sessionAge);
+        sessionSleep = tryParseInt(getServletConfig().getInitParameter("sessionSleep"), sessionSleep);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        int sessions = sessionManager.getActiveUserCount(getSessionAge());
+        int delay = sessions * getSessionSleep();
+        if (delay > getSleepLimit()) {
+            log.info(String.format("Sleep has become capped at: %d with %d sessions.", getSleepLimit(), sessions));
+            delay = getSleepLimit();
+        }
+        try {
+            Thread.sleep(delay);
+            resp.setStatus(HttpServletResponse.SC_OK);
+            // Return the number of sessions to aid debugging.
+            resp.getOutputStream().print(sessions);
+        } catch (InterruptedException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            log.warn("Got woken up: "+ e.getMessage());
+        }
+    }
+
+    private int getSleepLimit() {
+        return sleepLimit;
+    }
+
+    private int getSessionAge() {
+        return sessionAge;
+    }
+
+    private int getSessionSleep() {
+        return sessionSleep;
+    }
+
+    /**
+     * Convert string to int.
+     * @param value The String to convert.
+     * @param defaultValue The default value to return if it can't be converted.
+     * @return The int value of a string.
+     */
+    int tryParseInt(String value, int defaultValue) {
+        try {
+            return Integer.parseInt(value);
+        } catch(NumberFormatException nfe) {
+            // Log exception.
+            return defaultValue;
+        }
+    }
+}
