0f7553e0feeb9ccf4dcd40920f06dbab18f7db08
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2015-07-21 10:18:31 +0100

    Add config options - Currently only shows elfinder for normal file browsing (not images) - No real connector, so no files are shown

--- reference/library/src/webapp/editor/elfinder/js/init.js
+++ reference/library/src/webapp/editor/elfinder/js/init.js
@@ -1,12 +1,90 @@
-// Initialize elFinder
-(function() {
+// Closure to protect our initialization properties and safely get jQuery
+(function($) {
 
-// Documentation for client options:
-// https://github.com/Studio-42/elFinder/wiki/Client-configuration-options
-$(document).ready(function() {
-  $('#elfinder').elfinder({
-    url : 'php/connector.minimal.php',  // connector URL (REQUIRED)
+var init = function() {
+  // General properties
+  var $window = $(window);
+  var $document = $(document);
+/*
+  // Get parameters from window's URL string
+  var parseGetQuery = function() {
+    var query = {};
+    var string = window.location.search.slice(1);
+    var params = string.split('&');
+    $.each(params, function(idx, param) {
+      var keyValue = param.split('=');
+      query[keyValue[0]] = keyValue[1];
+    });
+
+    return query;
+  };
+
+  var query = parseGetQuery();
+  var funcNum = query.CKEditorFuncNum;
+  var langCode = query.langCode;
+  var type = query.type;
+*/
+  var onlyMimes = [];
+/*
+  if (type == 'image') {
+    onlyMimes.push('image');
+  } else if (type == 'flash') {
+    onlyMimes.push('application/x-shockwave-flash');
+  }*/
+
+  // Initialize elFinder
+  var $elfinder = $('#elfinder').elfinder({
+    // Connector script (replace with Java connector for Sakai)
+    url : 'php/sakaiExampleConnector.php',
+
+    // When a file is clicked, its data will be sent back to the editor that
+    // instantiated it, and this window will close
+    getFileCallback : function(file) {
+      window.opener.CKEDITOR.tools.callFunction(funcNum, file);
+      window.close();
+    },
+
+    // Restrict to certain file types
+    onlyMimes: onlyMimes,
+
+    // Dimensions
+    height: $window.height(),
+
+    // Buttons available on the toolbar
+    uiOptions: {
+      toolbar : [
+        ['help'],
+        ['back', 'forward'],
+        ['reload'],
+        ['home', 'up'],
+        ['mkdir', 'upload'],
+        ['open', 'download', 'getfile'],
+        ['info'],
+        ['quicklook'],
+        ['copy', 'cut', 'paste'],
+        ['search'],
+        ['view'],
+      ],
+    },
+
+    // Fullscreen editor, so no resizing
+    resizable: false,
   });
-});
 
-})();
+  var $instance = $elfinder.elfinder('instance');
+
+  // Auto resizing
+  // Solution by @oyejorge on
+  // https://github.com/Studio-42/elFinder/issues/84
+  $window.resize(function() {
+    var winHeight = $window.height() - 2; // Accomodate for top/bottom borders
+    if ($elfinder.height() !== winHeight) {
+      $elfinder.height(winHeight).resize();
+    }
+  });
+};
+
+// Run when the document is ready
+$(init);
+
+})(jQuery);
