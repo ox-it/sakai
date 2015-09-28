cac2d4d85f7f29bc42892bb7df9957e4bb6db2b6
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2015-09-28 17:47:52 +0100

    WL-4105 Allow 8 character paper codes.
    This is because exams now have longer paper codes, exam codes remain the same length.

--- tool/src/java/uk/ac/ox/oucs/oxam/components/CodeTextFilter.java
+++ tool/src/java/uk/ac/ox/oucs/oxam/components/CodeTextFilter.java
@@ -8,8 +8,17 @@ public class CodeTextFilter<E> extends TextFilter<E> {
 
 	private static final long serialVersionUID = 1L;
 
-	public CodeTextFilter(String id, IModel<E> model, FilterForm<?> form) {
+	/**
+	 * Create a code filter.
+	 * @param id component id
+	 * @param model model for the underlying form component
+	 * @param form filter form this filter will be added to
+	 * @param i The size of the text box.
+	 */
+	public CodeTextFilter(String id, IModel<E> model, FilterForm<?> form, int i) {
 		super(id, model, form);
+		String value = Integer.toString(i);
+		getFilter().add(new InputLengthLimiter(i));
 	}
 
 }
---
+++ tool/src/java/uk/ac/ox/oucs/oxam/components/InputLengthLimiter.java
@@ -0,0 +1,29 @@
+package uk.ac.ox.oucs.oxam.components;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.AbstractBehavior;
+import org.apache.wicket.behavior.IBehavior;
+import org.apache.wicket.markup.ComponentTag;
+
+/**
+ * Simple behaviour to limit the length of inputs to a hard max.
+ */
+public class InputLengthLimiter extends AbstractBehavior implements IBehavior {
+
+    private final int length;
+
+    public InputLengthLimiter(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public void onComponentTag(final Component component, final ComponentTag tag)
+    {
+        if (isEnabled(component))
+        {
+            String length = Integer.toString(this.length);
+            tag.getAttributes().put("maxlength", length);
+            tag.getAttributes().put("length", length);
+        }
+    }
+}
