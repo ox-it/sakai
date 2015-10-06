a58bba70de47d0745d0f7be5d496c25328a135d3
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2015-10-06 10:15:15 +0100

    WL-4126 Added test for the department browser.
    This tests the top level of the hierarchy on my local machine and takes 3 seconds most of the time so on a heavily loaded LDAP server it could easily bump over the 5 seconds limit.

--- integration-tests/src/test/java/uk/ac/ox/oucs/vle/TestExternalGroups.java
+++ integration-tests/src/test/java/uk/ac/ox/oucs/vle/TestExternalGroups.java
@@ -1,15 +1,22 @@
 package uk.ac.ox.oucs.vle;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.sakaiproject.memory.api.Cache;
+import org.sakaiproject.memory.api.MemoryService;
 import org.sakaiproject.user.api.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 
 public class TestExternalGroups extends AbstractDependencyInjectionSpringContextTests {
 
-	private ExternalGroupManager groupManager;
+	private ExternalGroupManagerImpl groupManager;
 
 	@Override
 	protected String[] getConfigLocations() {
@@ -18,7 +25,35 @@ public class TestExternalGroups extends AbstractDependencyInjectionSpringContext
 	
 	protected void onSetUp() {
 		ApplicationContext context = getApplicationContext();
-		groupManager = (ExternalGroupManager)context.getBean("ExternalGroupManager");
+		groupManager = (ExternalGroupManagerImpl) context.getBean("ExternalGroupManager");
+
+		Cache cache = Mockito.mock(Cache.class);
+		final Map<Object, Object> map = new HashMap<>();
+		Mockito.doAnswer(new Answer() {
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+				Object[] arguments = invocation.getArguments();
+				map.put(arguments[0], arguments[1]);
+				return Void.TYPE;
+			}
+		}).when(cache).put(Mockito.any(), Mockito.any());
+		Mockito.doAnswer(new Answer() {
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+				Object[] arguments = invocation.getArguments();
+				return map.get(arguments[0]);
+			}
+		}).when(cache).get(Mockito.any());
+
+		MemoryService memoryService = Mockito.mock(MemoryService.class);
+		Mockito.when(memoryService.newCache(Mockito.anyString())).thenReturn(cache);
+
+		groupManager.setMemoryService(memoryService);
+
+		MappedGroupDao mappedGroupDao = Mockito.mock(MappedGroupDao.class);
+		groupManager.setMappedGroupDao(mappedGroupDao);
+
+		groupManager.init();
 	}
 	
 	public void testSearch() throws Exception {
@@ -37,8 +72,11 @@ public class TestExternalGroups extends AbstractDependencyInjectionSpringContext
 			count++;
 		}
 		assertTrue(count > 0);
-	}	
-	
+	}
 
+	public void testBrowseCourseDepartments() throws Exception {
+		List<ExternalGroupNode> groups = groupManager.findNodes(ExternalGroupManagerImpl.COURSES);
+		assertFalse(groups.isEmpty());
+	}
 
 }
