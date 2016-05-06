c2f0421c789d95815a1e9fd8526bb552272ea31d
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2016-05-06 16:41:53 +0100

    WL-469: implemented a better strategy for removing middle initial(s).
    This implementation is as per Matthew's original suggestion. Here we
    extract the first name from the display name by removing the surname. If
    the surname is not present - unlikely, but possible - we just resort to the
    value of the givenName attribute from Oak LDAP.
    
    WL-652 Don't trust input LDAP data.
    Should prevent IOBE with firstname == lastname.

--- providers/jldap/src/java/edu/amc/sakai/user/OakLdapAttributeMapper.java
+++ providers/jldap/src/java/edu/amc/sakai/user/OakLdapAttributeMapper.java
@@ -2,39 +2,23 @@ package edu.amc.sakai.user;
 
 import org.sakaiproject.user.api.UserEdit;
 
-public class OakLdapAttributeMapper extends SimpleLdapAttributeMapper {
-    static final String OAK_PREFIX = "oakPrimaryPrincipal="; 
-    static final String KRB_PREFIX = "krbPrincipalName="; 
-    static final String SUFFIX 
-        = "@OX.AC.UK,cn=OX.AC.UK,cn=KerberosRealms,dc=oak,dc=ox,dc=ac,dc=uk";
-    static final String OAK_EMAIL = "oakAlternativeMail"; 
-    
-    /** 
-     * Get an LDAP filter suitable for the Oxford Oak LDAP.
-     * @returns an LDAP filter suitable for the Oxford Oak LDAP.
-     */
-    public String getFindUserByEidFilter(String eid) {
-        return OAK_PREFIX + KRB_PREFIX + eid + SUFFIX; 
-    }
+public class OakLdapAttributeMapper extends MultipleEmailLdapAttributeMapper {
 
     public void mapUserDataOntoUserEdit( LdapUserData userData,
         UserEdit userEdit ) {
         super.mapUserDataOntoUserEdit( userData, userEdit );
-        // Override the value set by the super class:
-        userEdit.setEid(oakPrimaryPrincipalToLoginName(userData.getEid()));
-    }
-    
-    private String oakPrimaryPrincipalToLoginName( String value ) {
-        return value.substring( KRB_PREFIX.length(), 
-            value.indexOf( SUFFIX.charAt( 0 ), KRB_PREFIX.length() ) );
+        // Override value set in superclass:
+        userEdit.setFirstName(firstName(userData));
+        String displayId = userEdit.getProperties().getProperty(JLDAPDirectoryProvider.DISPLAY_ID_PROPERTY);
+        if (displayId == null || displayId.length() == 0) {
+        	userEdit.getProperties().addProperty(JLDAPDirectoryProvider.DISPLAY_ID_PROPERTY, "none");
+        }
     }
 
-    /**
-     * Overridden for the Oxford Oak LDAP. This method ensures that we search
-     * e-mail using the multi-valued {@link OAK_EMAIL} attribute, which always
-     * includes the "official" Oxford e-mail address.
-     */
-    public String getFindUserByEmailFilter( String emailAddr ) {
-        return OAK_EMAIL + "=" + escapeSearchFilterTerm(emailAddr);
+    private String firstName( LdapUserData ud ) {
+        int i = (ud.getFirstName()!= null)?ud.getFirstName().lastIndexOf( " "+ud.getLastName() ):-1;
+        return (i > 0) ? ud.getFirstName().substring( 0, i ) 
+                        : ud.getProperties().getProperty( "givenName" );
     }
+
 }
