b7ef790369836a1bc7676a57cab90a07021bd9ea
Nick Wilson <nick.wilson@it.ox.ac.uk>
2016-05-13 13:26:42 +0100

    WL-1209,1223 : Display m.ox URLs alongside regular URLs in poll tool
    Steve Swinsburg patch
    
    WL-1209,1223
    
    Deleting comments
    
    WL-1209,1223 : Make QR popup work with JQuery 1.9.1

--- polls/tool/src/webapp/js/polls.js
+++ polls/tool/src/webapp/js/polls.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     //A filter to extract the real date formatted as a simple number and pass that to the sorter
     var dateExtractor = function(_that) {
-       var that = $(_that);
+        var that = $(_that);
         if(that.attr('name') && (that.attr('name').search(/realDate:/) != -1)){
             return that.attr('name').replace(/realDate:/,'').replace(/-/g,' ').replace(':00.0','').replace(/:/g,'').replace(/ /g,'');
         }
-       return that.text();
+        return that.text();
     }
     $("#sortableTable").tablesorter({
         sortList: [[3,1]], // Initial order by the latest (NOT earliest) closing date
@@ -19,55 +19,78 @@ $(document).ready(function() {
         },
         textExtraction: dateExtractor
     });
-    
+
     //register click events for the mobile info section
-    $(".mobile-info-link").click(function(){ 
-    	var id = $(this).attr('rel');
-    	
-    	shortenUrl("/direct/oxford/shorten?path=/poll/" + id + ".json");
-    	
-    	//get the shortened URL
-    	function shortenUrl(url) {
-    		
-	    	$.ajax({
-				url: url,
-				type: "GET",
-				cache: true,
-				dataType: "text",
-				timeout: 5000,
-				success: function(data) {
-		    		//set shortenedUrl into link
-		    		$("#dialog-mox-url").html(data);
-		    		//$("#dialog-mox-url").attr("href", data); //need it to be linked? if so, change to an <a> tag and uncomment this
-		    		
-		    		//also set img tag for QR code
-		    		var imgUrl = "https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=" + data;
-		    		$('#dialog-qr-code').attr("src", imgUrl);
-		    		
-		    		//and show the dialog
-		    		showDialog();
-				},
-				error: function(xhr, status) {
-					alert("Failed to retrieve m.ox url for poll: " + id + ", error: " + xhr.status);
-				}
-			});
-    	}
-    	
-    	
-    	function showDialog() {
-    		$("#dialog").dialog({
-    			resizable: false,
-    			draggable: true
-    		});
-    		//I haven't specified a position here so it is more flexible
-    		//i.e. click the first one, move the dialog, click the second, appears in same spot as the moved one.
-    		//If you specify a position it will reset to that position on subsequent clicks.
-    	}
-    	
-    	
-    	return false;
+    $(".mobile-info-link").click(function(){
+        var id = $(this).attr('rel');
+
+        shortenUrl("/poll/" + id + ".json");
+
+        return false;
     });
 
-    
-    
+    function shortenUrl(fullUrl) {
+
+        var ebUrl = '/direct/oxford/shorten?path=' + fullUrl;
+
+        $.ajax({
+            url: ebUrl,
+            type: "GET",
+            cache: true,
+            dataType: "text",
+            timeout: 5000,
+            success: function(data) {
+                //set shortenedUrl into link
+                $("#dialog-mox-url").html(data);
+
+                //also set img tag for QR code
+                var imgUrl = "/direct/oxford/qr?height=547&width=547&s=" + data;
+                $('#dialog-qr-code').attr("src", imgUrl);
+
+                //and show the dialog
+                showDialog();
+            },
+            error: function(xhr, status) {
+                alert("Failed to retrieve m.ox url for poll: " + id + ", error: " + xhr.status);
+            }
+        });
+    }
+
+
+    function showDialog() {
+        $("#dialog").dialog({
+            close: function(event, ui){
+                resizeFrame('shrink');
+            },
+            height: 680,
+            width: 580,
+            resizable: false,
+            draggable: true,
+            closeOnEscape: true
+        });
+        //I haven't specified a position here so it is more flexible
+        //i.e. click the first one, move the dialog, click the second, appears in same spot as the moved one.
+        //If you specify a position it will reset to that position on subsequent clicks.
+
+        //resize iframe
+        resizeFrame('grow');
+    }
+
+
+
+    function resizeFrame(updown){
+        if (top.location != self.location) {
+            var frame = parent.document.getElementById(window.name);
+        }
+        if (frame) {
+            if (updown == 'shrink') {
+                var clientH = document.body.clientHeight;
+            }
+            else {
+                var clientH = document.body.clientHeight + 450;
+            }
+            $(frame).height(clientH);
+        }
+    }
+
 });
