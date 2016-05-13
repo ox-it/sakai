93cfba25f9633469d9f61e4658b1c701003862a6
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2016-05-13 14:54:42 +0100

    Switch directtool to use hierarchy tool urls.

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/DirectToolHandler.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/DirectToolHandler.java
@@ -183,7 +183,7 @@ public class DirectToolHandler extends BasePortalHandler
 				if (session.getUserId() == null)
 				{
 					// let the tool do the the work (forward)
-					StoredState ss = portalService.newStoredState("directtool", "tool");
+					StoredState ss = portalService.newStoredState("directtool", "hierarchytool");
 					ss.setRequest(req);
 					ss.setPlacement(siteTool);
 					ss.setToolContextPath(toolContextPath);
@@ -202,7 +202,7 @@ public class DirectToolHandler extends BasePortalHandler
 			}
 		}
 		// let the tool do the the work (forward)
-		StoredState ss = portalService.newStoredState("directtool", "tool");
+		StoredState ss = portalService.newStoredState("directtool", "hierarchytool");
 		ss.setRequest(req);
 		ss.setPlacement(siteTool);
 		ss.setToolContextPath(toolContextPath);
