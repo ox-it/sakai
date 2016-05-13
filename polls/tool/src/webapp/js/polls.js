a9ed832416c59f9f74549a71f7bb72295d75c76f
Nick Wilson <nick.wilson@it.ox.ac.uk>
2016-05-13 13:25:16 +0100

    WL-1223 Update polls UI to include m.ox URL/QR.
    Conflicts:
    	tool/src/webapp/css/poll_base.css
    	tool/src/webapp/templates/votePolls.html

--- polls/tool/src/webapp/js/polls.js
+++ polls/tool/src/webapp/js/polls.js
@@ -8,13 +8,66 @@ $(document).ready(function() {
        return that.text();
     }
     $("#sortableTable").tablesorter({
-        sortList: [[2,1]], // Initial order by the latest (NOT earliest) closing date
+        sortList: [[3,1]], // Initial order by the latest (NOT earliest) closing date
         headers: { // disable it by setting the property sorter to false
-            1: {sorter: 'digit'},
+            0: {sorter: 'digit'},
+            1: {sorter: false},
             2: {sorter: 'digit'},
-            3: {sorter: false},
-            4: {sorter: false}
+            3: {sorter: 'digit'},
+            4: {sorter: false},
+            5: {sorter: false}
         },
         textExtraction: dateExtractor
     });
+    
+    //register click events for the mobile info section
+    $(".mobile-info-link").click(function(){ 
+    	var id = $(this).attr('rel');
+    	
+    	shortenUrl("/direct/oxford/shorten?path=/poll/" + id + ".json");
+    	
+    	//get the shortened URL
+    	function shortenUrl(url) {
+    		
+	    	$.ajax({
+				url: url,
+				type: "GET",
+				cache: true,
+				dataType: "text",
+				timeout: 5000,
+				success: function(data) {
+		    		//set shortenedUrl into link
+		    		$("#dialog-mox-url").html(data);
+		    		//$("#dialog-mox-url").attr("href", data); //need it to be linked? if so, change to an <a> tag and uncomment this
+		    		
+		    		//also set img tag for QR code
+		    		var imgUrl = "http://chart.apis.google.com/chart?chs=250x250&cht=qr&chl=" + data;
+		    		$('#dialog-qr-code').attr("src", imgUrl);
+		    		
+		    		//and show the dialog
+		    		showDialog();
+				},
+				error: function(xhr, status) {
+					alert("Failed to retrieve m.ox url for poll: " + id + ", error: " + xhr.status);
+				}
+			});
+    	}
+    	
+    	
+    	function showDialog() {
+    		$("#dialog").dialog({
+    			resizable: false,
+    			draggable: true
+    		});
+    		//I haven't specified a position here so it is more flexible
+    		//i.e. click the first one, move the dialog, click the second, appears in same spot as the moved one.
+    		//If you specify a position it will reset to that position on subsequent clicks.
+    	}
+    	
+    	
+    	return false;
+    });
+
+    
+    
 });
