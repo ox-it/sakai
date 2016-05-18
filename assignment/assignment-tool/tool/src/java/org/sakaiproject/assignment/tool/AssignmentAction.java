c96dac54f98547f0cb5d992228935176d5f4a3ff
Nick Wilson <nick.wilson@it.ox.ac.uk>
2016-05-18 14:06:58 +0100

    WL-3466 / WL-3455 : Change wording of Turnitin warning message

--- assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -9618,6 +9618,9 @@ public class AssignmentAction extends PagedResourceActionII
 			String uiService = ServerConfigurationService.getString("ui.service", "Sakai");
 			String[] args = new String[]{contentReviewService.getServiceName(), uiService, e.toString()};
             state.setAttribute("alertMessage", rb.getFormattedMessage("content_review.error.createAssignment", args));
+	        String technicalEmail = ServerConfigurationService.getString("feedback.technicalAddress", null);
+	        String alertMessage = rb.getFormattedMessage("content_review.error.createAssignment", new Object[]{technicalEmail});
+	        state.setAttribute("alertMessage", alertMessage);
         }
 		return false;
     }
