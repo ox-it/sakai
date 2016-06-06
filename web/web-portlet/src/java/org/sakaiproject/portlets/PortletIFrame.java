4fb2b2d0e20eb4166776c6af739c5d50b178e7d6
Nick Wilson <nick.wilson@it.ox.ac.uk>
2016-06-06 17:30:01 +0100

    WL-3017 Removed hostname from site-info urls while storing in the DB.
    And appended the server url in the site-info url on the edit screen for
    home page.
    Implemented the review comments.
    
    WL-3017 Fixed the home tool for empty infourl.
    
    Added nullcheck for infoUrl in the processActionEdit method of Portlet
    Iframe class.

--- web/web-portlet/src/java/org/sakaiproject/portlets/PortletIFrame.java
+++ web/web-portlet/src/java/org/sakaiproject/portlets/PortletIFrame.java
@@ -603,6 +603,11 @@ public class PortletIFrame extends GenericPortlet {
                                             if(infoUrl.startsWith("/") && !infoUrl.contains("://")){
                                                 infoUrl = ServerConfigurationService.getServerUrl() + infoUrl;
                                             }
+						    //Check if infoUrl is relative? and prepend the server url
+						    String serverUrl = ServerConfigurationService.getServerUrl();
+						    if(infoUrl.startsWith("/") && infoUrl.indexOf("://") == -1){
+						        infoUrl = serverUrl + infoUrl;
+						    }
 						    context.put("info_url", FormattedText.escapeHtmlFormattedTextarea(infoUrl));
 					    }
 
@@ -877,13 +882,19 @@ public class PortletIFrame extends GenericPortlet {
             if (SPECIAL_WORKSITE.equals(special))
             {
                 //Check info-url for null and empty
-                if(StringUtils.isNotBlank(infoUrl)){
-                    // If the site info url has server url then make it a relative link.
-                    String serverName = new URL(ServerConfigurationService.getServerUrl()).getHost();
-                    // if the supplied url starts with protocol//serverName:port/
-                    Pattern serverUrlPattern = Pattern.compile(String.format("^(https?:)?//%s:?\\d*/", serverName));
-                    infoUrl = serverUrlPattern.matcher(infoUrl).replaceFirst("/");
-                }
+	            //Check info-url for null and empty
+	            if(infoUrl != null && !infoUrl.equals("")) {
+		            // If the site info url has server url then make it a relative link.
+		            Collection<String> serverNames = new ArrayList<String>();
+		            //get the server name
+		            serverNames.add(new URL(ServerConfigurationService.getServerUrl()).getHost());
+		            serverNames.addAll(ServerConfigurationService.getInstance().getServerNameAliases());
+		            for (String serverName : serverNames) {
+			            // if the supplied url starts with protocol//serverName:port/
+			            Pattern serverUrlPattern = Pattern.compile(String.format("^(https?:)?//%s:?\\d*/", serverName));
+			            infoUrl = serverUrlPattern.matcher(infoUrl).replaceFirst("/");
+		            }
+	            }
                 String description = StringUtils.trimToNull(request.getParameter("description"));
                 //Need to save this processed
                 description = FormattedText.processFormattedText(description,new StringBuilder());
