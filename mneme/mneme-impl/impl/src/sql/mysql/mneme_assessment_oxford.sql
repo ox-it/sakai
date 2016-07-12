821d5b628576d74e2548965066f79f5bc0c73fb4
bgarciaentornos <bgarcia@entornosdeformacion.com>
2016-07-12 09:32:23 +0200

    Oxford dabatase script and solve migration bug

---
+++ mneme/mneme-impl/impl/src/sql/mysql/mneme_assessment_oxford.sql
@@ -0,0 +1,5 @@
+-----------------------------------------------------------------------------
+-- Mneme Assessment DDL changes from Oxford version
+-----------------------------------------------------------------------------
+
+ALTER TABLE MNEME_ASSESSMENT ADD (PASS_MARK FLOAT, SEND_SUBMIT_EMAIL CHAR (1));
