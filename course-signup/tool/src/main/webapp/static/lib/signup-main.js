63469429ad15e8cdf86e031df0912a97fc370eb5
Rebecca Miller <rebecca.miller@it.ox.ac.uk>
2016-09-01 15:10:52 +0100

    WL-4526 Fixed issues due to conflicts during previous merge.

--- course-signup/tool/src/main/webapp/static/lib/signup-main.js
+++ course-signup/tool/src/main/webapp/static/lib/signup-main.js
@@ -365,6 +365,10 @@ var Signup = function(){
 						if (noteOriginal == current.val()) {
 							current.after('<span class="error">* please enter some reasons for your choice</span>');
 						}
+					})
+
+					$("textarea[name=specialReq]", signupConfirm).bind("change", function(e){
+						return true;
 					});
 
 					signupConfirm.submit(function(event){
@@ -404,7 +408,7 @@ var Signup = function(){
 							modal: true,
 							stack: true,
 							position: position,
-							width: 600, // Would be nice to get inner content width.
+							width: 800, // Would be nice to get inner content width.
 							close: function(event, ui){
 								signupDialog.remove(); // Tidy up the DOM.
 							}
@@ -732,8 +736,10 @@ var Signup = function(){
 				}).join(" / ");
 			},
 			/**
-			 * Formats a notes string so we only display the first bit of it and then display a tooltip for the rest.
+			 * Formats a String so that if any special requirements have been added they are displayed first in red and other comments in a tooltip.
+			 * If there are no special requirements but there are notes then the first bit of the notes are displayed and the rest in a tooltip.
 			 * @param {Object} notes
+			 * @param {Object} specialReq
 			 */
 			"formatNotes": function(notes, specialReq){
 				if (specialReq) {
@@ -1118,7 +1124,7 @@ var Signup = function(){
 							if (allowChangeAction) {
 								actions = Signup.signup.formatActions(Signup.signup.getActions(this.status, this.id, closes, isAdmin));
 							}
-							data.push([this.id, (this.created) ? this.created : "", Signup.user.render(this.user, this.group, this.components), course, Signup.supervisor.render(this.supervisor, this, isAdmin), Signup.signup.formatNotes(this.notes), this.status, actions, this.status, starts]);
+							data.push([this.id, (this.created) ? this.created : "", Signup.user.render(this.user, this.group, this.components), course, Signup.supervisor.render(this.supervisor, this, isAdmin), Signup.signup.formatNotes(this.notes, this.specialReq), this.status, actions, this.status, starts]);
 
 						});
 						fnCallback({
