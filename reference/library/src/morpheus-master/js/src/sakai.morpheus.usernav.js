7696488f21a1cf9303c2394a98f2ebcf85206680
Nick Wilson <nick.wilson@it.ox.ac.uk>
2016-11-07 15:31:28 +0000

    WL-4643: Long lists of Favourite sites do not wrap in MSIE

--- reference/library/src/morpheus-master/js/src/sakai.morpheus.usernav.js
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.usernav.js
@@ -44,6 +44,14 @@ function toggleUserNav(event){
 $PBJQ(".js-toggle-user-nav a#loginUser > .Mrphs-userNav__drop-btn", "#loginLinks").on("click", toggleUserNav);
 $PBJQ(".js-toggle-user-nav .Mrphs-userNav__drop-btn", "#loginLinks").on("click", toggleUserNav);
 
+var ie11flex = function(){
+    if( !(window.ActiveXObject) && "ActiveXObject" in window && $PBJQ(window).width() >= 800 ){ // Failback for IE11
+      _list_width = $PBJQ(window).width() - $PBJQ('#mastLogin').outerWidth() - $PBJQ('.Mrphs-headerLogo').outerWidth();
+      $PBJQ('#Mrphs-sites-nav').css( 'width', ( _list_width  ) + 'px' );
+      $PBJQ('.Mrphs-sitesNav__menu','#Mrphs-sites-nav').css( 'width', ( _list_width ) + 'px' );
+    }
+}
+
 $PBJQ(document).ready( function(){
   if($PBJQ('.Mrphs-siteHierarchyImg').length>0 && $PBJQ(window).width() > 800){
     var margin = (parseInt($PBJQ('.Mrphs-siteHierarchy').outerHeight(true)) / 2);
@@ -55,5 +63,7 @@ $PBJQ(document).ready( function(){
   if( $PBJQ('.Mrphs-hierarchy--parent-sites').length > 0 && $PBJQ(window).width() <= 800 ){
     $PBJQ('#content').css( 'margin-top', ( parseInt( $PBJQ('#content').css('margin-top').replace('px', '') ) +  $PBJQ('.Mrphs-hierarchy--parent-sites').outerHeight(true) ) + 'px' );
   }
+  ie11flex();
 });
 
+$PBJQ(window).on('resize', function(){ ie11flex(); });
