e38cfccd49f0e198604104ca9565bdafa511b829
bbailla2 <bbailla2@uwo.ca>
2018-03-23 17:22:12 -0400

    OWL-3341

--- reset-pass/account-validator-tool/src/java/org/sakaiproject/accountvalidator/tool/otp/AcountValidationLocator.java
+++ reset-pass/account-validator-tool/src/java/org/sakaiproject/accountvalidator/tool/otp/AcountValidationLocator.java
@@ -19,7 +19,7 @@
  */
 package org.sakaiproject.accountvalidator.tool.otp;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.joda.time.DateTime;
