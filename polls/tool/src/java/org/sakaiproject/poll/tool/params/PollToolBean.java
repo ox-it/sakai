2b7585091c941cc0becd4d2e49f1513a0d957659
Brian Jones <bjones86@uwo.ca>
2018-10-12 14:38:42 -0400

    OWL-3503: Polls > creation date is never populated

--- polls/tool/src/java/org/sakaiproject/poll/tool/params/PollToolBean.java
+++ polls/tool/src/java/org/sakaiproject/poll/tool/params/PollToolBean.java
@@ -24,6 +24,7 @@ package org.sakaiproject.poll.tool.params;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -144,14 +145,14 @@ public class PollToolBean {
 			LOG.debug("Actualy updating poll " + poll.getPollId());
 			isNew = false;
 			//check for possible unchanged values
-			LOG.debug(" newPoll is " + poll.getText()+ " while poll text is " + poll.getText());
-			
+			Poll existingPoll = manager.getPollById(poll.getPollId(), false);
+			LOG.debug(" newPoll is " + poll.getText()+ " while poll text is " + existingPoll.getText());
 
-			if (poll.getText().equals("") && poll.getText()!=null)
-				poll.setText(poll.getText());
-
-			if (poll.getDetails().equals("") && poll.getDetails() != null)
-				poll.setDetails(poll.getDetails());
+			if (poll.getCreationDate() == null) {
+				poll.setCreationDate(existingPoll.getCreationDate());
+			}
+		} else {
+			poll.setCreationDate(new Date());
 		}
 
 		
