9d1c88f88bf1a3eaf06886ca3e47e02c5712773b
Brian Jones <bjones86@uwo.ca>
2019-08-13 14:39:30 -0400

    WL-5325 SAK-42333

--- reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/ConfirmProducer.java
+++ reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/ConfirmProducer.java
@@ -1,6 +1,6 @@
 package org.sakaiproject.tool.resetpass;
 
-
+import org.apache.commons.lang.StringUtils;
 import org.sakaiproject.component.api.ServerConfigurationService;
 import org.sakaiproject.tool.api.Placement;
 import org.sakaiproject.tool.api.ToolManager;
@@ -15,11 +15,12 @@ import uk.org.ponder.rsf.viewstate.ViewParameters;
 
 public class ConfirmProducer implements ViewComponentProducer {
 	public static final String VIEW_ID = "confirm";
+
 	private ToolManager toolManager;
 	public void setToolManager(ToolManager toolManager) {
 		this.toolManager = toolManager;
 	}
-	
+
 	public String getViewID() {
 		return VIEW_ID;
 	}
@@ -28,32 +29,31 @@ public class ConfirmProducer implements ViewComponentProducer {
 	public void setServerConfigurationService(ServerConfigurationService s) {
 		this.serverConfigurationService = s;
 	}
-	
+
 	private RetUser userBean;
 	public void setUserBean(RetUser u){
 		this.userBean = u;
 	}
-	
-	public void fillComponents(UIContainer tofill, ViewParameters arg1,
-			ComponentChecker arg2) {
-		
-		String[] parms = new String[] {userBean.getEmail()};
+
+	public void fillComponents(UIContainer tofill, ViewParameters arg1, ComponentChecker arg2) {
+
 		boolean validatingAccounts = serverConfigurationService.getBoolean("siteManage.validateNewUsers", true);
 		
 		if (!validatingAccounts) {
-			UIMessage.make(tofill,"message","confirm",parms);
+			UIMessage.make(tofill,"message", "confirm", new String[] {userBean.getEmail()});
 		} else {
-			UIMessage.make(tofill,"message","confirm.validate");
+			UIMessage.make(tofill,"message", "confirm.validate", new String[] {serverConfigurationService.getString("ui.service", "Sakai"), userBean.getEmail()});
 		}
+
 		// Get the instructions from the tool placement.
 		Placement placement = toolManager.getCurrentPlacement();
 		String supportInstructions = placement == null ? "" :  placement.getConfig().getProperty("supportInstructions");
-		if(supportInstructions != null && !"".equals(supportInstructions)){
+		if(StringUtils.isNotBlank(supportInstructions)){
 			UIVerbatim.make(tofill, "supportMessage", supportInstructions);
 		}else if (serverConfigurationService.getString("mail.support", null) != null) {
+			String supportEmail = serverConfigurationService.getString("mail.support", "");
 			UIMessage.make(tofill, "supportMessage", "supportMessage");
-			UILink.make(tofill, "supportEmail",serverConfigurationService.getString("mail.support", ""),"mailto:" + serverConfigurationService.getString("mail.support", ""));
+			UILink.make(tofill, "supportEmail", supportEmail, "mailto:" + supportEmail);
 		}
 	}
-
 }
