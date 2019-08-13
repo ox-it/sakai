ce81c96fda25af773271c1427e654da2718b9f6e
Brian Jones <bjones86@uwo.ca>
2019-08-13 14:06:03 -0400

    OWL-3628

--- reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/UserValidator.java
+++ reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/UserValidator.java
@@ -5,52 +5,39 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sakaiproject.component.api.ServerConfigurationService;
 import org.sakaiproject.user.api.User;
 import org.sakaiproject.user.api.UserDirectoryService;
 import org.sakaiproject.authz.api.SecurityService;
-import org.sakaiproject.tool.api.Placement;
-import org.sakaiproject.tool.api.ToolManager;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 public class UserValidator implements Validator {
 
 	private static Logger m_log  = LoggerFactory.getLogger(UserValidator.class);
+	public String userEmail;
 
-	// prefix for error messages - indicates they are to be pulled from tool configuration rather than a resource bundle
-	private final String TOOL_CONFIG_PREFIX = "toolconfig_";
-	
 	public boolean supports(Class clazz) {
 		return clazz.equals(User.class);
 	}
 
-	public String userEmail;
-	
-	
 	private ServerConfigurationService serverConfigurationService;
 	public void setServerConfigurationService(ServerConfigurationService s) {
 		this.serverConfigurationService = s;
 	}
-	
+
 	private UserDirectoryService userDirectoryService;
 	public void setUserDirectoryService(UserDirectoryService ds){
 		this.userDirectoryService = ds;
 	}
-	
+
 	private SecurityService securityService;
 	public void setSecurityService(SecurityService ss){
 		this.securityService = ss;
 	}
 
-	private ToolManager toolManager;
-	public void setToolManager(ToolManager tm) {
-		this.toolManager = tm;
-	}
-	
 	public void validate(Object obj, Errors errors) {
 		RetUser retUser = (RetUser)obj;
 		m_log.debug("validating user " + retUser.getEmail());
@@ -61,25 +48,30 @@ public class UserValidator implements Validator {
 			errors.reject("noemailprovided", "no email provided");
 			return;
 		}
-		
+
 		Collection<User> c = this.userDirectoryService.findUsersByEmail(retUser.getEmail().trim());
 		if (c.size()>1) {
+			// Email is tied to more than one user, null out the user and transfer to next page
 			m_log.warn("more than one account with email: {}", retUser.getEmail());
-			errors.reject("morethanone","more than one email");
+			retUser.setUser(null);
 			return;
 		} else if (c.size()==0) {
+			// User doesn't exist, null out the user and transfer to next page
 			m_log.debug("no such email: {}", retUser.getEmail());
-			errors.reject("nosuchuser","no such user");
+			retUser.setUser(null);
 			return;
 		}
+
 		Iterator<User> i = c.iterator();
 		User user = (User)i.next();
 		m_log.debug("got user " + user.getId() + " of type " + user.getType());
 		if (securityService.isSuperUser(user.getId())) {
+			// Email belongs to super user, null out the user and transfer to next page
 			m_log.warn("tryng to change superuser password");
-			rejectWrongType(errors);
+			retUser.setUser(null);
 			return;
 		}
+
 		boolean allroles = serverConfigurationService.getBoolean("resetPass.resetAllRoles",false);
 		if (!allroles){
 			// SAK-24379 - deprecate the resetRoles property
@@ -93,36 +85,18 @@ public class UserValidator implements Validator {
 					roles = rolesOld;
 				}
 			}
-		    if (roles == null ){
-		        roles = new String[]{"guest"};
-		    }
-		    List<String> rolesL = Arrays.asList(roles);
-		    if (!rolesL.contains(user.getType())) {
-		        m_log.warn("this is a type don't change");
-		        rejectWrongType(errors);
-		        return;
-		    }
+			if (roles == null ){
+				roles = new String[]{"guest"};
+			}
+			List<String> rolesL = Arrays.asList(roles);
+			if (!rolesL.contains(user.getType())) {
+				// Email belongs to a user who's type is not allowed to use this tool, null out the user and transfer to the next page
+				m_log.warn("this is a type don't change");
+				retUser.setUser(null);
+				return;
+			}
 		}
+
 		retUser.setUser(user);
 	}
-
-	/**
-	 * Explains that the user's type is incorrect.
-	 * Looks for a custom message in the tool properties first,
-	 * if there is no custom message, it goes to the message bundle
-	 */
-	private void rejectWrongType(Errors errors)
-	{
-		Placement placement = toolManager.getCurrentPlacement();
-		String toolPropWrongType = placement.getConfig().getProperty("wrongtype");
-		if (StringUtils.isBlank(toolPropWrongType))
-		{
-			errors.reject("wrongtype", "wrong type");
-		}
-		else
-		{
-			errors.reject(TOOL_CONFIG_PREFIX + "wrongtype", toolPropWrongType);
-		}
-	}
-
 }
