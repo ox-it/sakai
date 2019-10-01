a6a3f6ebacf3cdf84860a7c83d676c6d0b355875
Brian Jones <bjones86@uwo.ca>
2019-10-01 12:02:04 -0400

    OWL-3658: reset-pass > reject invalid domains

--- reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/UserValidator.java
+++ reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/UserValidator.java
@@ -2,15 +2,21 @@ package org.sakaiproject.tool.resetpass;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+
 import org.sakaiproject.component.api.ServerConfigurationService;
 import org.sakaiproject.user.api.User;
 import org.sakaiproject.user.api.UserDirectoryService;
 import org.sakaiproject.authz.api.SecurityService;
+import org.sakaiproject.tool.api.Placement;
+import org.sakaiproject.tool.api.ToolManager;
+
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
@@ -18,6 +24,18 @@ public class UserValidator implements Validator {
 
 	private static Logger m_log  = LoggerFactory.getLogger(UserValidator.class);
 	public String userEmail;
+
+	// Prefix for error messages - indicates they are to be pulled from tool configuration rather than a resource bundle
+	private static final String TOOL_CONFIG_PREFIX = "toolconfig_";
+
+	// Sakai.property key for invalid domains in reset password email requests
+	private static final String SAK_PROP_INVALID_EMAIL_DOMAINS = "resetPass.invalidEmailDomains";
+
+	// Message bundle key for wrong type message (invalid domain)
+	private static final String WRONG_TYPE_MSG_BUNDLE_KEY = "wrongtype";
+
+	// Message bundle key for no email provided message
+	private static final String NO_EMAIL_MSG_BUNDLE_KEY = "noemailprovided";
 
 	public boolean supports(Class clazz) {
 		return clazz.equals(User.class);
@@ -38,65 +56,62 @@ public class UserValidator implements Validator {
 		this.securityService = ss;
 	}
 
+	private ToolManager toolManager;
+	public void setToolManager(ToolManager tm) {
+		this.toolManager = tm;
+	}
+
 	public void validate(Object obj, Errors errors) {
 		RetUser retUser = (RetUser)obj;
 		m_log.debug("validating user " + retUser.getEmail());
 
-		if (retUser.getEmail() == null || "".equals(retUser.getEmail()))
-		{
+		// Short circuit: no email provided
+		if (StringUtils.isBlank(retUser.getEmail())) {
 			m_log.debug("no email provided");
-			errors.reject("noemailprovided", "no email provided");
+			errors.reject(NO_EMAIL_MSG_BUNDLE_KEY, "no email provided");
 			return;
 		}
 
+		// Short circuit: domain provided not allowed
+		List<String> invalidDomains = Arrays.asList(serverConfigurationService.getStrings(SAK_PROP_INVALID_EMAIL_DOMAINS));
+		if (CollectionUtils.isNotEmpty(invalidDomains)) {
+			if (invalidDomains.stream().anyMatch( d -> retUser.getEmail().toLowerCase().contains(d.toLowerCase()))) {
+				Placement placement = toolManager.getCurrentPlacement();
+				String toolPropWrongType = placement.getConfig().getProperty(WRONG_TYPE_MSG_BUNDLE_KEY);
+				if (StringUtils.isBlank(toolPropWrongType)) {
+					errors.reject(WRONG_TYPE_MSG_BUNDLE_KEY, "wrong type");
+				} else {
+					errors.reject(TOOL_CONFIG_PREFIX + WRONG_TYPE_MSG_BUNDLE_KEY, toolPropWrongType);
+				}
+
+				return;
+			}
+		}
+
+		// User doesn't exist, null out the user and transfer to next page
 		Collection<User> c = this.userDirectoryService.findUsersByEmail(retUser.getEmail().trim());
-		if (c.size()>1) {
-			// Email is tied to more than one user, null out the user and transfer to next page
-			m_log.warn("more than one account with email: {}", retUser.getEmail());
-			retUser.setUser(null);
-			return;
-		} else if (c.size()==0) {
-			// User doesn't exist, null out the user and transfer to next page
+		if (CollectionUtils.isEmpty(c)) {
 			m_log.debug("no such email: {}", retUser.getEmail());
 			retUser.setUser(null);
 			return;
 		}
 
-		Iterator<User> i = c.iterator();
-		User user = (User)i.next();
-		m_log.debug("got user " + user.getId() + " of type " + user.getType());
+		// Email is tied to more than one user, null out the user and transfer to next page
+		else if (c.size() > 1) {
+			m_log.warn("more than one account with email: {}", retUser.getEmail());
+			retUser.setUser(null);
+			return;
+		}
+
+		// Email belongs to super user, null out the user and transfer to next page
+		User user = (User) c.iterator().next();
 		if (securityService.isSuperUser(user.getId())) {
-			// Email belongs to super user, null out the user and transfer to next page
 			m_log.warn("tryng to change superuser password");
 			retUser.setUser(null);
 			return;
 		}
 
-		boolean allroles = serverConfigurationService.getBoolean("resetPass.resetAllRoles",false);
-		if (!allroles){
-			// SAK-24379 - deprecate the resetRoles property
-			String[] roles = serverConfigurationService.getStrings("accountValidator.accountTypes.accept");
-			String[] rolesOld = serverConfigurationService.getStrings("resetRoles");
-			if (rolesOld != null)
-			{
-				m_log.warn("Found the resetRoles property; it is deprecated in favour of accountValidator.accountTypes.accept");
-				if (roles == null)
-				{
-					roles = rolesOld;
-				}
-			}
-			if (roles == null ){
-				roles = new String[]{"guest"};
-			}
-			List<String> rolesL = Arrays.asList(roles);
-			if (!rolesL.contains(user.getType())) {
-				// Email belongs to a user who's type is not allowed to use this tool, null out the user and transfer to the next page
-				m_log.warn("this is a type don't change");
-				retUser.setUser(null);
-				return;
-			}
-		}
-
+		// All checks have passed successfully
 		retUser.setUser(user);
 	}
 }
