1ca483d6b6c1373abefb08ccf3fe024b1601caf3
bbailla2 <bbailla2@uwo.ca>
2021-01-05 10:28:19 -0500

    SAK-3638 - merged the 20.1 to 20.2 conversion for mysql and oracle

--- config/sql/113-owl51-to-20x-owl1-mysql-conversion.sql
+++ config/sql/113-owl51-to-20x-owl1-mysql-conversion.sql
@@ -1425,3 +1425,23 @@ UPDATE IGNORE rbc_evaluation re
 
 DROP TABLE messages_with_rubric;
 -- END SAK-43441
+
+
+-- BEGIN 20.1 -> 20.2
+
+-- SAK-44420
+UPDATE poll_option SET deleted = 0 WHERE DELETED IS NULL;
+ALTER TABLE poll_option MODIFY COLUMN DELETED BIT NOT NULL DEFAULT 0;
+-- End SAK-44420
+
+-- SAK-43497
+alter table ASN_ASSIGNMENT_PROPERTIES modify VALUE varchar(4000) null;
+-- END SAK-43407
+
+-- SAK-44636 - Add LTI Lessons Placement checkbox - By default it is off for future tool installations
+ALTER TABLE lti_tools ADD pl_lessonsselection TINYINT DEFAULT 0;
+-- Existing records needed to be switched on
+UPDATE lti_tools SET pl_lessonsselection = 1;
+-- END SAK-44636
+
+-- END 20.1 -> 20.2
