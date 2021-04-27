3c2c32b534c3ed7128ed579e0a6748bb9922bb00
Paul Lukasewych <plukasew@uwo.ca>
2021-04-27 07:53:31 -0400

    [OWL-4438] Gradebook cell menus not keyboard accessible

--- gradebookng/tool/src/webapp/scripts/gradebook-gbgrade-table.js
+++ gradebookng/tool/src/webapp/scripts/gradebook-gbgrade-table.js
@@ -2689,7 +2689,7 @@ GbGradeTable.setupKeyboardNavigation = function() {
 
         $dropdown.dropdown("toggle");
         setTimeout(function() {
-          $(".dropdown-menu:visible a:first").focus();
+          $(".dropdown-menu:visible li:not(.hidden):first a").focus();
         });
       }
 
