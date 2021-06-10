084d5d0477a070b37d0f7249756c1de6b26a132a
Paul Lukasewych <plukasew@uwo.ca>
2021-06-10 10:17:04 -0400

    [OWL-2847] Clean up error message when editing a question type

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/ItemConfigBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/ItemConfigBean.java
@@ -439,11 +439,15 @@ public class ItemConfigBean implements Serializable {
       list.add(new SelectItem("10", getResourceDisplayName("import_from_q")));
     }
 
-    List<SelectItem> ret = new ArrayList<SelectItem>();
-    ret.add(new SelectItem("", getResourceDisplayName("select_qtype")));
-    ret.addAll(list);
-    
-    return ret;
+    return list;
+  }
+
+  public List<SelectItem> getAddItemTypeSelectList()
+  {
+    List<SelectItem> list = getItemTypeSelectList();
+    list.add(0, new SelectItem("", getResourceDisplayName("select_qtype")));
+
+    return list;
   }
 
   /**
