0df7bae1face491ea729306004a2b1708a5e4eab
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2022-09-09 14:58:53 +0100

    WL-5368 Drop the termdates support.

--- termdates/api/src/main/java/uk/ac/ox/oucs/termdates/exception/WeekNameNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.ox.oucs.termdates.exception;
-
-/**
- * @author Colin Hebert
- */
-public class WeekNameNotFoundException extends RuntimeException {
-}
--- termdates/impl/src/test/java/uk/ac/ox/oucs/termdates/AbstractTermConverterServiceTest.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ox.oucs.termdates;
-
-/**
- * @author Colin Hebert
- */
-public abstract class AbstractTermConverterServiceTest extends TermConverterServiceTest {
-    @Override
-    public abstract AbstractTermConverterService getTermConverterService();
-}
