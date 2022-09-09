24537643195b78ff110f728d9ecaaa17c720dcc0
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2022-09-09 14:19:03 +0100

    WL-5368 Remove course signup
    This is no longer used and doesn’t need to be in the build any more.

--- course-signup/tool/src/main/java/uk/ac/ox/oucs/vle/resources/Views.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ox.oucs.vle.resources;
-
-/**
- * These are marker classes that are used in JSON serialisation to limit the properties and nested objects
- * that are returned.
- */
-public class Views {
-    static public class Flat {}
-}
--- course-signup/tool/src/test/java/uk/ac/ox/oucs/vle/UserResourceTest.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.ox.oucs.vle;
-
-/**
- * Created by buckett on 15/10/15.
- */
-public class UserResourceTest {
-}
